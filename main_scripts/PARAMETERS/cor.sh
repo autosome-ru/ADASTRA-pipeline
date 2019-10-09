57ce3a7be438ee38ceed033af63dabcda81fa2dd
M1kss <boytsovs.av@phystech.edu>
2019-10-09 21:43:18 +0300

    80 jobs

--- main_scripts/PARAMETERS/cor.sh
+++ main_scripts/PARAMETERS/cor.sh
@@ -6,11 +6,11 @@ ScriptsFolder="/home/abramov/ASB-Project/main_scripts/"
 flag=$1
 
 if [ "$flag" == --ploidy ]; then
-	parallel --jobs 40 python3 "$ScriptsFolder"PLOIDYcalling/PloidyEstimation.py :::: "$ParametersListsFolder"/PE_parameters.cfg
+	parallel --jobs 80 python3 "$ScriptsFolder"PLOIDYcalling/PloidyEstimation.py :::: "$ParametersListsFolder"/PE_parameters.cfg
 fi
 
 python3 "$ScriptsFolder"PARAMETERS/MakeParametersForASWP.py
 
-parallel --jobs 40 python3 "$ScriptsFolder"CORRELATIONanalysis/Annotate_SNPs_with_ploidy.py :::: "$ParametersListsFolder"/ASWP_parameters.cfg
+parallel --jobs 10 python3 "$ScriptsFolder"CORRELATIONanalysis/Annotate_SNPs_with_ploidy.py :::: "$ParametersListsFolder"/ASWP_parameters.cfg
 
 python3 "$ScriptsFolder"CORRELATIONanalysis/CorStats.py
