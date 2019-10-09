7b7eac8214d12131cb742ff28a331796e91d56ee
M1kss <boytsovs.av@phystech.edu>
2019-10-09 21:09:47 +0300

    citation

--- main_scripts/PARAMETERS/cor.sh
+++ main_scripts/PARAMETERS/cor.sh
@@ -6,13 +6,11 @@ ScriptsFolder="/home/abramov/ASB-Project/main_scripts/"
 flag=$1
 
 if [ "$flag" == --ploidy ]; then
-	parallel --citation --jobs 40 python3 "$ScriptsFolder"PLOIDYcalling/PloidyEstimation.py :::: "$ParametersListsFolder"/PE_parameters.cfg
+	parallel --jobs 40 python3 "$ScriptsFolder"PLOIDYcalling/PloidyEstimation.py :::: "$ParametersListsFolder"/PE_parameters.cfg
 fi
 
 python3 "$ScriptsFolder"PARAMETERS/MakeParametersForASWP.py
 
-echo "made parameters"
-
-parallel --citation --jobs 40 python3 "$ScriptsFolder"CORRELATIONanalysis/Annotate_SNPs_with_ploidy.py :::: "$ParametersListsFolder"/ASWP_parameters.cfg
+parallel --jobs 40 python3 "$ScriptsFolder"CORRELATIONanalysis/Annotate_SNPs_with_ploidy.py :::: "$ParametersListsFolder"/ASWP_parameters.cfg
 
 python3 "$ScriptsFolder"CORRELATIONanalysis/CorStats.py
