249ef1161547db40aa9677ebe8cd2c22b4da80fa
wish1 <aswq22013@gmail.com>
2019-10-09 20:33:53 +0300

    cor fix

--- main_scripts/PARAMETERS/cor.sh
+++ main_scripts/PARAMETERS/cor.sh
@@ -5,12 +5,12 @@ ScriptsFolder="/home/abramov/ASB-Project/main_scripts/"
 
 flag=$1
 
-if [ "$flag" == Ploidy ]; then
-	parallel --jobs 1 python3 "$ScriptsFolder"PLOIDYcalling/PloidyEstimation.py :::: "$ParametersListsFolder"/PE_parameters.cfg
+if [ "$flag" == --ploidy ]; then
+	parallel --jobs 40 python3 "$ScriptsFolder"PLOIDYcalling/PloidyEstimation.py :::: "$ParametersListsFolder"/PE_parameters.cfg
 fi
 
 python3 "$ScriptsFolder"PARAMETERS/MakeParametersForASWP.py
 
-parallel --jobs 1 python3 "$ScriptsFolder"CORRELATIONanalysis/Annotate_SNPs_with_ploidy.py :::: "$ParametersListsFolder"/ASWP_parameters.cfg
+parallel --jobs 40 python3 "$ScriptsFolder"CORRELATIONanalysis/Annotate_SNPs_with_ploidy.py :::: "$ParametersListsFolder"/ASWP_parameters.cfg
 
 python3 "$ScriptsFolder"CORRELATIONanalysis/CorStats.py
