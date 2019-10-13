8d59039ebf13e83e90bba560b088dfa42e78cded
M1kss <boytsovs.av@phystech.edu>
2019-10-14 02:27:10 +0300

    join threads

--- main_scripts/PARAMETERS/cor.sh
+++ main_scripts/PARAMETERS/cor.sh
@@ -11,12 +11,12 @@ fi
 
 if [ "$flag" == --ploidy ] || [ "$flag" == --aswp ]; then
   python3 "$ScriptsFolder"PARAMETERS/MakeParametersForASWP.py
-
+  python3 "$ScriptsFolder"PARAMETERS/SortParameters.py
   parallel --jobs 40 python3 "$ScriptsFolder"CORRELATIONanalysis/Annotate_SNPs_with_ploidy.py :::: "$ParametersListsFolder"/ASWP_parameters.cfg
 fi
 
 python3 "$ScriptsFolder"PARAMETERS/MakeParametersForCS.py
-
+python3 "$ScriptsFolder"PARAMETERS/SortParameters.py
 parallel --jobs 40 python3 "$ScriptsFolder"CORRELATIONanalysis/CorStats.py :::: "$ParametersListsFolder"/CS_parameters.cfg
 
 python3 "$ScriptsFolder"CORRELATIONanalysis/JoinThreads.py
