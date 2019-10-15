0f0009aea2ae5e01052f1d321da21fc8e989ad73
wish1 <aswq22013@gmail.com>
2019-10-15 23:41:55 +0300

    fix

--- main_scripts/PARAMETERS/ploidy_est.sh
+++ main_scripts/PARAMETERS/ploidy_est.sh
@@ -3,5 +3,5 @@
 ParametersListsFolder="/home/abramov/ParallelParameters/"
 ScriptsFolder="/home/abramov/ASB-Project/main_scripts/"
 
-python3 python3 "$ScriptsFolder"PARAMETERS/MakeParametersForPE.py
+python3 "$ScriptsFolder"PARAMETERS/MakeParametersForPE.py
 parallel --jobs 80 python3 "$ScriptsFolder"PLOIDYcalling/PloidyEstimation.py :::: "$ParametersListsFolder"/PE_parameters.cfg
