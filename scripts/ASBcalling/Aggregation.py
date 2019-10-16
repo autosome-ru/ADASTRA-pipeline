d6fefaf5bfd7094dae627555eac5927ad3947d38
wish1 <aswq22013@gmail.com>
2019-10-16 22:11:38 +0300

    aggggggregation

--- scripts/ASBcalling/Aggregation.py
+++ scripts/ASBcalling/Aggregation.py
@@ -25,7 +25,7 @@ def unpack(line):
     else:
         p_ref, p_alt = map(float, line[19:21])
 
-    return chr, pos, ID, ref, alt, ref_c, alt_c, Q, GQ, in_macs, in_sissrs, in_cpics, in_gem, callers, ploidy,\
+    return chr, pos, ID, ref, alt, ref_c, alt_c, Q, GQ, in_macs, in_sissrs, in_cpics, in_gem, callers, ploidy, \
            dip_qual, lq, rq, seg_c, p_ref, p_alt
 
 
@@ -33,7 +33,7 @@ def pack(values):
     return '\t'.join(map(str, values)) + '\n'
 
 
-def annotate_snp_with_tables(dictionary, pandas_table):  # return part of the dictionary with fdr from table
+def annotate_snp_with_tables(dictionary, df):  # return part of the dictionary with fdr from table
     pass
 
 
@@ -41,12 +41,22 @@ def get_name(path):  # path format */ALIGNS000000_table_p.txt
     return path.split("/")[-1].split["_"][0]
 
 
-results_path = '/home/abramov/TF_P-values/'
+expected_args = {"CL", "TF"}
+
+results_path = '/home/abramov/DATA/'
 parameters_path = '/home/abramov/PARAMETERS/'
 dicts_path = '/home/abramov/DATA/DICTS/'
 
-what_for = sys.argv[1]
+what_for = sys.argv[1]  # "TF" or "CL" arguments are expected
+if what_for not in expected_args:
+    exit(1)
 key = sys.argv[2]
+
+if not os.path.isdir(results_path + what_for + '_DICTS/'):
+    os.mkdir(results_path + what_for + '_DICTS/')
+if not os.path.isdir(results_path + what_for + "_P-values/"):
+    os.mkdir(results_path + what_for + "_P-values/")
+
 with open(parameters_path + what_for + "_DICT.json", "r") as read_file:
     d = json.loads(read_file.readline())  # read CL or TF json
 tables = d.get(key, None)
@@ -73,7 +83,7 @@ for table in tables:
 
 print('Writing ', key)
 
-with open(results_path + key + '_common_table.tsv', 'w') as out:
+with open(results_path + what_for + "_P-values/" + key + '_common_table.tsv', 'w') as out:
     out.write(pack(['#chr', 'pos', 'ID', 'ref', 'alt', 'm_callers', 'm_ploidy', 'm_q', 'm_dipq',
                     'm_segc', 'm_datasets', 'maxdepth_ref/alt', 'maxdepth_ploidy', 'maxdepth_m1',
                     'maxdepth_m2', 'mostsig_ref/alt', 'mostsig_ploidy', 'mostsig_m1', 'mostsig_m2',
@@ -201,7 +211,7 @@ with open(results_path + key + '_common_table.tsv', 'w') as out:
         origin_of_snp_dict[",".join(map(str, key))] = c_table_names
 out.close()
 print("Counting FDR")
-with open(results_path + key + '_common_table.tsv', 'r') as f:
+with open(results_path + what_for + "_P-values/" + key + '_common_table.tsv', 'r') as f:
     table = pd.read_table(f)
     f.close()
     table["m_fdr"] = table[["m_fpref", "m_fpalt"]].min(axis=1)
@@ -209,9 +219,9 @@ with open(results_path + key + '_common_table.tsv', 'r') as f:
                                                                alpha=0.05, method='fdr_bh')
     table["m_fdr"] = pd.Series(p_val)
     print(bool_ar)
-    with open(results_path + key + '_common_table.tsv', "w") as w:
+    with open(results_path + what_for + "_P-values/" + key + '_common_table.tsv', "w") as w:
         table.to_csv(w, sep="\t", index=False)
     table = table.loc(bool_ar)
     datasets_for_SNPs = annotate_snp_with_tables(origin_of_snp_dict, table)
-    with open(dicts_path + key + '_DICT.json', 'w') as out:
+    with open(results_path + what_for + '_DICTS/' + key + '_DICT.json', 'w') as out:
         json.dump(datasets_for_SNPs, out)
