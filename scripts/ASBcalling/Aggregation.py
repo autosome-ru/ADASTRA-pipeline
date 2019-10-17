e1e9d96c802eb96ab30a78d8307b9a9bba7c58d0
wish1 <aswq22013@gmail.com>
2019-10-17 12:57:02 +0300

    test

--- scripts/ASBcalling/Aggregation.py
+++ scripts/ASBcalling/Aggregation.py
@@ -276,6 +276,9 @@ if __name__ == '__main__':
     with open(results_path + what_for + "_P-values/" + key_name + '_common_table.tsv', 'r') as f:
         table = pd.read_table(f)
         f.close()
+        a = statsmodels.stats.multitest.multipletests(table["m_fpref"],
+                                                                           alpha=0.05, method='fdr_bh')
+        print(a)
         bool_ar_ref, p_val_ref = statsmodels.stats.multitest.multipletests(table["m_fpref"],
                                                                            alpha=0.05, method='fdr_bh')
         bool_ar_alt, p_val_alt = statsmodels.stats.multitest.multipletests(table["m_fpralt"],
