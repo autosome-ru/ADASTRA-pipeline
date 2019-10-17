8acd26b845030588c56977b5cda4e168a96a71ec
wish1 <aswq22013@gmail.com>
2019-10-18 01:11:57 +0300

    Removed Reverse cell lines dict

--- scripts/PARAMETERS/MakeParametersForPvC.py
+++ scripts/PARAMETERS/MakeParametersForPvC.py
@@ -4,13 +4,25 @@ import os.path
 parameters_path = '/home/abramov/PARAMETERS/'
 parallel_parameters_path = '/home/abramov/ParallelParameters/'
 
-if __name__ == "__main__":
-    with open(parameters_path + 'REVERSE_CELL_LINES.json', 'r') as read_file:
-        d = json.loads(read_file.readline())
 
-    keys = sorted(d.keys())
+def make_reverse_dict(dictionary):
+    new_dict = {}
+    for key in dictionary:
+        paths = dictionary[key]
+        for path in paths:
+            if path.split("/")[-3] != "CTRL":
+                new_dict[path] = key
+    return new_dict
+
+
+if __name__ == "__main__":
+    with open(parameters_path + 'CELL_LINES.json', 'r') as read_file:
+        d = json.loads(read_file.readline())
+        rev_d = make_reverse_dict(d)
+    keys = sorted(rev_d.keys())
+
     with open(parallel_parameters_path + 'PvC_parameters.cfg', 'w') as file:
-        for key in keys:
-            full_path = ".".join(key.split(".")[:-2])
-            if os.path.isfile(key) and os.path.isfile(full_path + "_table_annotated.txt"):
+        for k in keys:
+            full_path = ".".join(k.split(".")[:-2])
+            if os.path.isfile(k) and os.path.isfile(full_path + "_table_annotated.txt"):
                 file.write(full_path + '\n')
--- scripts/PARAMETERS/aggregation.sh
+++ scripts/PARAMETERS/aggregation.sh
@@ -6,11 +6,11 @@ ScriptsFolder="/home/abramov/ASB-Project/scripts/"
 indicator=$1
 njobs=$2
 
-if [ $indicator == "--forCL" ];then
+if [ "$indicator" == "--forCL" ];then
   python3 "$ScriptsFolder"PARAMETERS/MakeParametersForAgr.py "CL"
   parallel --jobs "$njobs" python3 "$ScriptsFolder"ASBcalling/Aggregation.py "CL" :::: "$ParametersListsFolder"/Agr_parameters.cfg
 fi
-if [ $indicator == "--forTF" ];then
+if [ "$indicator" == "--forTF" ];then
   python3 "$ScriptsFolder"PARAMETERS/MakeParametersForAgr.py "TF"
   parallel --jobs "$njobs" python3 "$ScriptsFolder"ASBcalling/Aggregation.py "TF" :::: "$ParametersListsFolder"/Agr_parameters.cfg
 fi
