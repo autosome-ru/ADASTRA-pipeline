8cd859dbf4c80d4ed8cec5dbb834fb16e0d5d2fd
wish1 <aswq22013@gmail.com>
2019-10-17 12:34:39 +0300

    some fixes2

--- scripts/PARAMETERS/MakeParametersForAgr.py
+++ scripts/PARAMETERS/MakeParametersForAgr.py
@@ -6,13 +6,12 @@ parallel_parameters_path = '/home/abramov/ParallelParameters/'
 
 
 if __name__ == "__main__":
-    what_for = sys.argv[1]
-    if what_for == "TF":
-        with open(parameters_path + 'TF_DICT.json', 'r') as read_file:
-            d = json.loads(read_file.readline())
-    elif what_for == "CL":
-        with open(parameters_path + 'CL_DICT.json', 'r') as read_file:
-            d = json.loads(read_file.readline())
+    expected_args = {"CL": "TF", "TF": "CL"}
+    what_for = sys.argv[1]  # "TF" or "CL" arguments are expected
+    if what_for not in expected_args:
+        raise ValueError('{} not in CL, TF'.format(what_for))
+    with open(parameters_path + what_for + '_DICT.json', 'r') as read_file:
+        d = json.loads(read_file.readline())
 
     keys = sorted(d.keys())
     with open(parallel_parameters_path + 'Agr_parameters.cfg', 'w') as file:
