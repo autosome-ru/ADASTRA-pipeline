dca2246e63a48a114f835a3892264b423d100fea
M1kss <boytsovs.av@phystech.edu>
2019-10-19 01:11:40 +0300

    for cor

--- scripts/PARAMETERS/cor.sh
+++ scripts/PARAMETERS/cor.sh
@@ -7,17 +7,17 @@ flag=$1
 
 if [ "$flag" == --ploidy ]; then
   python3 "$ScriptsFolder"PARAMETERS/SortParameters.py
-	parallel --jobs 80 python3 "$ScriptsFolder"PLOIDYcalling/PloidyEstimation.py :::: "$ParametersListsFolder"/PE_parameters.cfg :::: "$ParametersListsFolder"/modes.cgf :::: "$ParametersListsFolder"/states.cfg :::: "$ParametersListsFolder"/b_pen.cfg :::: "$ParametersListsFolder"/d_pen.cfg
+	parallel --jobs 80 python3 "$ScriptsFolder"PLOIDYcalling/PloidyEstimation.py :::: "$ParametersListsFolder"PE_parameters.cfg :::: "$ParametersListsFolder"modes.cgf :::: "$ParametersListsFolder"states.cfg :::: "$ParametersListsFolder"b_pen.cfg :::: "$ParametersListsFolder"d_pen.cfg
 fi
 
 if [ "$flag" == --ploidy ] || [ "$flag" == --aswp ]; then
   python3 "$ScriptsFolder"PARAMETERS/MakeParametersForASWP.py
   python3 "$ScriptsFolder"PARAMETERS/SortParameters.py
-  parallel --jobs 80 python3 "$ScriptsFolder"CORRELATIONanalysis/Annotate_SNPs_with_ploidy.py :::: "$ParametersListsFolder"/ASWP_parameters.cfg
+  parallel --jobs 80 python3 "$ScriptsFolder"CORRELATIONanalysis/Annotate_SNPs_with_ploidy.py :::: "$ParametersListsFolder"ASWP_parameters.cfg
 fi
 
 python3 "$ScriptsFolder"PARAMETERS/MakeParametersForCS.py
 python3 "$ScriptsFolder"PARAMETERS/SortParameters.py
-parallel --jobs 80 python3 "$ScriptsFolder"CORRELATIONanalysis/CorStats.py :::: "$ParametersListsFolder"/CS_parameters.cfg
+parallel --jobs 80 python3 "$ScriptsFolder"CORRELATIONanalysis/CorStats.py :::: "$ParametersListsFolder"CS_parameters.cfg
 
 python3 "$ScriptsFolder"CORRELATIONanalysis/JoinThreads.py
