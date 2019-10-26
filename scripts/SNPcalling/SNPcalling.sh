39ba856cac4879f10f446fbf283ab4ce314a772d
wish1 <aswq22013@gmail.com>
2019-10-26 12:19:16 +0300

    fixes

--- scripts/SNPcalling/SNPcalling.sh
+++ scripts/SNPcalling/SNPcalling.sh
@@ -3,7 +3,7 @@ source ../HELPERS/Config.cfg
 
 VCF="/home/abramov/REFERENCE/00-common_all.vcf.gz"
 REFERENCE="/home/abramov/REFERENCE/"
-
+FA=${REFERENCE}"genome-norm.fasta"
 
 while [ "$(echo "$1" | cut -c1)" = "-" ]
 do
@@ -29,32 +29,27 @@ if [ ! -f "$VCF.tbi" ]; then
        		"-F $VCF"
 fi
 
-
-FA=${REFERENCE}"genome-norm.fasta"
-
 if [  -f "$BAMPATH/$BAMNAME.bam.bai" ]; then
 	rm "$BAMPATH/$BAMNAME.bam.bai"
 fi	
 echo "Index file for $BAMPATH/$BAMNAME"
-samtools index "$BAMPATH/$BAMNAME.bam"
-if [ $? != 0 ]; then
+
+if ! samtools index "$BAMPATH/$BAMNAME.bam"
+then
         echo "Failed to index bam"
         exit 1
 fi
 
-	
 echo "Cutting bam.."
-samtools view -b "$BAMPATH/$BAMNAME.bam" \
+if ! samtools view -b "$BAMPATH/$BAMNAME.bam" \
 	chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 \
 	chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY > "$OUT/${BAMNAME}_chop.bam"
-
-if [ $? != 0 ]; then
+then
     echo "Failed to cut bam"
     exit 1
 fi
-
 # shellcheck disable=SC2154
-$Java "$JavaParameters" -jar "$PICARD" \
+if ! $Java "$JavaParameters" -jar "$PICARD" \
 	AddOrReplaceReadGroups \
 	I="$OUT/${BAMNAME}_chop.bam" \
 	O="$OUT/${BAMNAME}_formated.bam" \
@@ -63,57 +58,51 @@ $Java "$JavaParameters" -jar "$PICARD" \
 	RGPL=seq1 \
 	RGPU=unit1 \
 	RGSM=20
-
-if [ $? != 0 ]; then
+then
     echo "Failed to picard.AddOrReplaceReadGroups"
     exit 1
 fi
 
-$Java "$JavaParameters" -jar "$PICARD" \
+if ! $Java "$JavaParameters" -jar "$PICARD" \
 	MarkDuplicates \
 	I="$OUT/${BAMNAME}_formated.bam" \
 	O="$OUT/${BAMNAME}_ready.bam" \
 	REMOVE_DUPLICATES=true \
 	M="$OUT/${BAMNAME}_metrics.txt"
-
-if [ $? != 0 ]; then
+then
     echo "Failed to picard.MarkDplicates"
     exit 1
 fi
 
-$Java "$JavaParameters" -jar "$GATK" \
+if ! $Java "$JavaParameters" -jar "$GATK" \
 	BaseRecalibrator \
 	-R "$FA" \
 	-I "$OUT/${BAMNAME}_ready.bam" \
 	-known-sites "$VCF" \
 	-O "$OUT/${BAMNAME}.table"
-
-if [ $? != 0 ]; then
+then
     echo "Failed to make base recalibration"
     exit 1
 fi
 
-$Java "$JavaParameters" -jar "$GATK" \
+if ! $Java "$JavaParameters" -jar "$GATK" \
 	ApplyBQSR \
 	-R "$FA" \
 	-I "$OUT/${BAMNAME}_ready.bam" \
 	--bqsr-recal-file "$OUT/${BAMNAME}.table" \
 	-O "$OUT/${BAMNAME}_final.bam"
-
-if [ $? != 0 ]; then
+then
     echo "Failed to apply BSQR"
     exit 1
 fi
 
-$Java "$JavaParameters" -jar "$GATK" \
+if ! $Java "$JavaParameters" -jar "$GATK" \
 	HaplotypeCaller \
 	-R "$FA" \
 	-I "$OUT/${BAMNAME}_final.bam" \
 	--dbsnp "$VCF" \
 	-O "$OUT/${BAMNAME}.vcf"
-
-
-if [ $? != 0 ]; then
+then
     echo "Failed gatk.HaplotypeCaller"
     exit 1
 fi
