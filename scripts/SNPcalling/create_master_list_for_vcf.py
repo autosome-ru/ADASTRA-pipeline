4d22b389fd0bbc348c51f4c874e3c2258495b7d3
M1kss <boytsovs.av@phystech.edu>
2019-11-03 13:05:44 +0300

    list for made vcfs

--- scripts/SNPcalling/create_master_list_for_vcf.py
+++ scripts/SNPcalling/create_master_list_for_vcf.py
@@ -1,10 +1,11 @@
 import sys
+import os
 
 sys.path.insert(1, "/home/abramov/ASB-Project")
 from scripts.HELPERS.paths import parameters_path
 from scripts.HELPERS.helpers import make_list_for_VCFs
 
-out_path = parameters_path + "MasterListForVCFs.tsv"
+out_path = parameters_path + "MasterListForVCFs_made.tsv"
 
 if __name__ == "__main__":
-    make_list_for_VCFs(out_path)
+    make_list_for_VCFs(out_path, condition_function=os.path.isfile)
