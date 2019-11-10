521c28eb74f34ced32e54503affff0179ef2176e
wish1 <aswq22013@gmail.com>
2019-11-11 01:31:14 +0300

    SNPcalling for 11 chr

--- scripts/PARAMETERS/snp_calling.sh
+++ scripts/PARAMETERS/snp_calling.sh
@@ -6,4 +6,4 @@ ScriptsFolder="/home/abramov/ASB-Project/scripts/"
 njobs=$1
 flag=$2
 
-parallel --delay 80 --jobs "$njobs" bash "$ScriptsFolder"SNPcalling/ProcessLine.sh "$flag" :::: "$ParametersPath"/BadVCF.tsv
+parallel --jobs "$njobs" bash "$ScriptsFolder"SNPcalling/ProcessLine.sh "$flag" :::: "$ParametersPath"/MasterListForVCFs.tsv
--- scripts/SNPcalling/SNPcalling.sh
+++ scripts/SNPcalling/SNPcalling.sh
@@ -43,9 +43,7 @@ then
 fi
 
 echo "Cutting bam.."
-if ! samtools view -b "$BamPath/$BamName.bam" \
-	chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 \
-	chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY > "$OutPath/${BamName}_chop.bam"
+if ! samtools view -b "$BamPath/$BamName.bam" chr11  > "$OutPath/${BamName}_chop.bam"
 then
     echo "Failed to cut bam"
     exit 1
