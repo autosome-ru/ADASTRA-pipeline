64f27569a640682554dad818d868f12394e73672
m1kss <boytsovs.av@phystech.edu>
2019-11-18 12:18:22 -0500

    censored binom

--- scripts/ASBcalling/Pcounter.py
+++ scripts/ASBcalling/Pcounter.py
@@ -7,9 +7,13 @@ from scripts.HELPERS.paths import ploidy_dict_path, create_ploidy_path_function
 from scripts.HELPERS.helpers import callers_names, unpack, pack, Intersection
 
 
-def count_p(x, n, p, alternative):
+def count_p(x, n, p, alternative, cut_off=2):
+    if x <= cut_off or x >= n - cut_off:
+        raise ValueError('Read-counts {} must be greater than cut off value {}'.format(x, cut_off))
+    tail = binom_test(cut_off, n, p, alternative='less') + binom_test(n - cut_off, n, p, alternative='greater')
     pv = (binom_test(x, n, p, alternative) + binom_test(x, n, 1 - p, alternative)) / 2
-    return pv
+    corrected_pv = 0.5 + (pv - 0.5) / (1 - tail)
+    return corrected_pv
 
 
 def make_reverse_dict(dictionary):
@@ -24,19 +28,19 @@ def make_reverse_dict(dictionary):
 
 if __name__ == '__main__':
     full_path = sys.argv[1]
-    
+
     key = full_path + ".vcf.gz"
     table_annotated = full_path + "_table_annotated.txt"
     output = full_path + "_table_p.txt"
-    
+
     with open(ploidy_dict_path, "r") as read_file:
         d = json.loads(read_file.readline())
         rev_d = make_reverse_dict(d)
-    
+
     ploidy = create_ploidy_path_function(rev_d[key])
-    
+
     print('Now doing {} \n with ploidy file {}'.format(table_annotated, rev_d[key]))
-    
+
     with open(ploidy, 'r') as ploidy_file, open(output, 'w') as out, open(table_annotated, 'r') as table_file:
         out.write(pack(['#chr', 'pos', 'ID', 'ref', 'alt', 'ref_read_counts', 'alt_read_counts',
                         'repeat_type'] + callers_names + ['BAD', 'Q1', 'left_qual', 'right_qual', 'SNP_count',
