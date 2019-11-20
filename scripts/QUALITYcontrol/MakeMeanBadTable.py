85865fa261357c42d8d27b994984a6679a63ab25
wish1 <aswq22013@gmail.com>
2019-11-21 00:53:27 +0300

    test

--- scripts/QUALITYcontrol/MakeMeanBadTable.py
+++ scripts/QUALITYcontrol/MakeMeanBadTable.py
@@ -7,21 +7,22 @@ from scripts.HELPERS.paths import parameters_path, ploidy_path
 from scripts.HELPERS.helpers import pack
 
 out_path = parameters_path + "cell_lines_BADs.tsv"
+actual_ploidy_path = ploidy_path + "Corrected-6/"
 
 if __name__ == "__main__":
     with open(out_path, "w") as out:
         sum_table = None
         previous_name = None
-        for file_name in sorted(os.listdir(ploidy_path)):
+        for file_name in sorted(os.listdir(actual_ploidy_path)):
             cell_line_name = file_name.split("!")[0]
             if previous_name == cell_line_name:
-                table = pd.read_table(ploidy_path + file_name)
+                table = pd.read_table(actual_ploidy_path + file_name)
                 sum_table = sum_table.append(table)
             else:
                 if sum_table is None:
                     sum_table = pd.read_table(ploidy_path + file_name)
                 else:
-                    print(sum_table)
+
                     mean_BAD = sum_table["BAD"].mean()
                     median_BAD = sum_table["BAD"].median()
                     out.write(pack([previous_name, mean_BAD, median_BAD]))
