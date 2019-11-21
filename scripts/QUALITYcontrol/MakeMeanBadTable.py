3a4698c694f8e2f986d3fb43f0b1675f86521397
wish1 <aswq22013@gmail.com>
2019-11-21 14:13:58 +0300

    fix

--- scripts/QUALITYcontrol/MakeMeanBadTable.py
+++ scripts/QUALITYcontrol/MakeMeanBadTable.py
@@ -11,8 +11,8 @@ out_path = parameters_path + "cell_lines_BADs.tsv"
 actual_ploidy_path = ploidy_path + "Corrected-6/"
 
 
-def write_BAD(out_buffer, pd_column, datasets_n):
-    out_buffer.write(pack([previous_name, pd_column.mean(), pd_column.median(), datasets_n, len(pd_column.index)]))
+def write_BAD(out_buffer, pd_column, datasets_n, SNP_n):
+    out_buffer.write(pack([previous_name, pd_column.mean(), pd_column.median(), datasets_n, SNP_n]))
 
 
 if __name__ == "__main__":
@@ -24,23 +24,25 @@ if __name__ == "__main__":
         previous_name = None
         for file_name in sorted(os.listdir(actual_ploidy_path)):
             cell_line_name = file_name.split("!")[0]
-            try:
-                cur_l = len([x for x in cell_lines_dict[file_name.split("_ploidy")[0]] if os.path.isfile(x)])
-            except KeyError:
-                cur_l = "Bug"
+            with open(ploidy_path + file_name.replace("_ploidy", "")) as f:
+                cur_SNP_number = len(pd.read_table(f).index)
+            cur_l = len([x for x in cell_lines_dict[file_name.split("_ploidy")[0]] if os.path.isfile(x)])
             with open(actual_ploidy_path + file_name) as file:
                 table = pd.read_table(file)
             if previous_name == cell_line_name:
                 sum_table = sum_table.append(table)
                 aligns_number += cur_l
+                SNP_number += cur_SNP_number
             else:
                 if sum_table is None:
                     sum_table = table
                     aligns_number = cur_l
                     previous_name = cell_line_name
+                    SNP_number = cur_SNP_number
                 else:
-                    write_BAD(out, sum_table["BAD"], aligns_number)
+                    write_BAD(out, sum_table["BAD"], aligns_number, SNP_number)
                     sum_table = table
                     aligns_number = cur_l
+                    SNP_number = cur_SNP_number
                     previous_name = cell_line_name
-        write_BAD(out, sum_table["BAD"], aligns_number)
+        write_BAD(out, sum_table["BAD"], aligns_number, SNP_number)
