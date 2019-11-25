7d29e0b4f1ae612b21589dc51683841105a98535
wish1 <aswq22013@gmail.com>
2019-11-25 21:08:12 +0300

    fix

--- scripts/QUALITYcontrol/CheckBADsSegmentation.py
+++ scripts/QUALITYcontrol/CheckBADsSegmentation.py
@@ -11,20 +11,6 @@ out_path = parameters_path + "cell_lines_BADs.tsv"
 actual_ploidy_path = ploidy_path + "Corrected-6-C-CAIC/"
 
 
-def write_BAD(out_buffer, pd_df, max_n, total_n, datasets_n, SNP_n, n_without_SNP, IDs):
-    print(total_n)
-    try:
-        mean_by_SNP = (pd_df["BAD"] * pd_df["SNP_count"]).sum()/pd_df["SNP_count"].sum()
-    except ZeroDivisionError:
-        mean_by_SNP = "nan"
-    try:
-        mean_by_bp = (pd_df["BAD"] * (pd_df["end"] - pd_df["start"])).sum()/(pd_df["end"] - pd_df["start"]).sum()
-    except ZeroDivisionError:
-        mean_by_bp = "nan"
-    out_buffer.write(pack([previous_name, mean_by_SNP, mean_by_bp, max_n, total_n,
-                           datasets_n, SNP_n, n_without_SNP, ",".join(map(str, IDs))]))
-
-
 if __name__ == "__main__":
     with open(out_path, "w") as out:
         sum_table = None
@@ -37,5 +23,5 @@ if __name__ == "__main__":
                         continue
                     line = line.split()
                     if int(line[7]) > 30000:
-                        print("here it comes xD {} ".format(file_name))
+                        print("here it comes xD {}, {} SNPs".format(file_name, line[7]))
                     out.write(pack([line[7], int(line[2])-int(line[1]), file_name]))
