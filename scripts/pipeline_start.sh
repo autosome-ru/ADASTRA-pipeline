2bf52e823f4e82438e4881909458e5c8d5d8b166
M1kss <boytsovs.av@phystech.edu>
2019-12-05 03:17:16 +0300

    pipeline_start.sh

--- scripts/pipeline_start.sh
+++ scripts/pipeline_start.sh
@@ -6,3 +6,17 @@
 # 3) Rename files with "_"
 # 4) Create "models_file" for ploidy model and integrate it with corstats
 # 5) move bash files from PARAMETERS dir
+
+ScriptsFolder="/home/abramov/ASB-Project/scripts/"
+
+njobs=$1
+flag=$2
+
+python3 "$ScriptsFolder"PARAMETERS/MakeParametersForPE.py
+if [ "$flag" == --pvalue ]; then
+  bash "$ScriptsFolder"PARAMETERS/p_value_count.sh "$njobs"
+fi
+
+bash "$ScriptsFolder"PARAMETERS/aggregation.sh "$njobs" --forTF
+
+bash "$ScriptsFolder"PARAMETERS/aggregation.sh "$njobs" --forCL
