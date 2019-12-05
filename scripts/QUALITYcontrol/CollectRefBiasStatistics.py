1b68cec186f779163021562acdd597175747610b
wish1 <aswq22013@gmail.com>
2019-12-06 00:03:19 +0300

    Collect statistics script added

---
+++ scripts/QUALITYcontrol/CollectRefBiasStatistics.py
@@ -0,0 +1,25 @@
+import sys
+import os.path
+import json
+import pandas as pd
+
+sys.path.insert(1, "/home/abramov/ASB-Project")
+from scripts.HELPERS.paths import cl_dict_path
+
+
+name = 'HUES64_embryonic_stem_cells'
+
+with open(cl_dict_path, "r") as read_file:
+    cell_lines_dict = json.loads(read_file.readline())
+sum_df = None
+for align_path in cell_lines_dict[name]:
+    if not os.path.isfile(align_path):
+        continue
+    df = pd.read_table(align_path)
+    if df.empty:
+        continue
+    if sum_df is None:
+        sum_df = df[['ref_read_counts', 'alt_read_counts']]
+    else:
+        sum_df.append(df[['ref_read_counts', 'alt_read_counts']])
+print(sum_df['ref_read_counts'].value_counts())
