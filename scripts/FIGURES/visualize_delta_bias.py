71d125e89e3e30b9b0a295dbf4fdabeb1b0678a4
wish1 <aswq22013@gmail.com>
2019-12-06 13:36:05 +0300

    added visualize delta_bias script

--- scripts/FIGURES/visualize_delta_bias.py
+++ scripts/FIGURES/visualize_delta_bias.py
@@ -4,15 +4,18 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-n = int(sys.argv[1])
-stats = pd.read_table('cover_bias_statistics.tsv')
+#n = int(sys.argv[1])
+n = 75
+stats = pd.read_table('~/cover_bias_statistics.tsv')
 stats = stats[stats['cover'] == n]
-stats['ref'] = (stats['counts'] + stats['delta']) / 2
-sns.barplot(stats[['ref', 'counts']])
+
+stats['ref'] = (stats['cover'] + stats['delta']) / 2
+sns.barplot(x='ref', y='counts', data=stats)
 
 plt.grid(True)
-plt.title('ref-alt bias for CELLLINE')
+plt.title('ref-alt bias for cell line n={}'.format(n))
 plt.ylabel('count')
-plt.xlabel('delta(ref-alt)')
-plt.savefig('bias_CELLLINE.png')
+plt.xlabel('ref_read_counts')
+plt.xticks([])
+#plt.savefig('ref-alt_bias_n={}.png'.format(n))
 plt.show()
