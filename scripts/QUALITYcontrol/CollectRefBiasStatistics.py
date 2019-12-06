744f96c125969cf0b3c2d351dfe82957b3c96fd5
M1kss <boytsovs.av@phystech.edu>
2019-12-06 05:43:52 +0300

    bias

--- scripts/QUALITYcontrol/CollectRefBiasStatistics.py
+++ scripts/QUALITYcontrol/CollectRefBiasStatistics.py
@@ -12,6 +12,7 @@ name = '*'
 with open(cl_dict_path, "r") as read_file:
     cell_lines_dict = json.loads(read_file.readline())
 sum_df = None
+out_t = pd.DataFrame()
 
 for key in cell_lines_dict:
     for align_path in cell_lines_dict[key]:
@@ -21,17 +22,22 @@ for key in cell_lines_dict:
         df = pd.read_table(align_path)
         if df.empty:
             continue
+        sum_df = df[['ref_read_counts', 'alt_read_counts']]
+
         if sum_df is None:
-            sum_df = df[['ref_read_counts', 'alt_read_counts']]
+            out_t['ref'] = sum_df['ref_read_counts'].value_counts()
+            out_t['alt'] = sum_df['alt_read_counts'].value_counts()
+            out_t['allele_reads'] = out_t.index
+            out_t = out_t.reset_index(drop=True)
+            out_t.fillna(0, inplace=True)
         else:
-            sum_df = sum_df.append(df[['ref_read_counts', 'alt_read_counts']])
+            tmp_df = pd.DataFrame()
+            tmp_df['ref'] = sum_df['ref_read_counts'].value_counts()
+            tmp_df['alt'] = sum_df['alt_read_counts'].value_counts()
+            tmp_df['allele_reads'] = tmp_df.index
+            tmp_df = tmp_df.reset_index(drop=True)
+            tmp_df.fillna(0, inplace=True)
+            out_t = out_t.append(tmp_df).groupby('allele_reads', as_index=False).sum()
 
 with open(parameters_path + 'bias_statistics.tsv', 'w') as out:
-    out_t = pd.DataFrame()
-    ser = sum_df['ref_read_counts'].value_counts()
-    out_t['ref'] = sum_df['ref_read_counts'].value_counts()
-    out_t['alt'] = sum_df['alt_read_counts'].value_counts()
-    out_t['allele_reads'] = out_t.index
-    out_t.reset_index(drop=True)
-    out_t.fillna(0, inplace=True)
     out_t.to_csv(out, sep="\t")
