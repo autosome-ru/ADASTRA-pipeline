5c5bf59497d51a992bd8333529e07403c469333b
M1kss <boytsovs.av@phystech.edu>
2019-12-06 04:29:39 +0300

    bias

--- scripts/QUALITYcontrol/CollectRefBiasStatistics.py
+++ scripts/QUALITYcontrol/CollectRefBiasStatistics.py
@@ -24,10 +24,14 @@ for align_path in cell_lines_dict[name]:
     else:
         sum_df = sum_df.append(df[['ref_read_counts', 'alt_read_counts']])
 with open(parameters_path + 'ref_statistics.tsv', 'w') as out:
-    out_t = sum_df['ref_read_counts'].value_counts()
-    out_t.columns = ['ref', 'count']
+    out_t = pd.DataFrame()
+    ser = sum_df['ref_read_counts'].value_counts()
+    out_t['ref'] = ser.index
+    out_t['count'] = ser
     out_t.to_csv(out, sep="\t")
 with open(parameters_path + 'alt_statistics.tsv', 'w') as out:
-    out_t = sum_df['alt_read_counts'].value_counts()
-    out_t.columns = ['alt', 'count']
+    out_t = pd.DataFrame()
+    ser = sum_df['alt_read_counts'].value_counts()
+    out_t['alt'] = ser.index
+    out_t['count'] = ser
     out_t.to_csv(out, sep="\t")
