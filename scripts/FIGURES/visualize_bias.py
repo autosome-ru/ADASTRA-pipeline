18cfc4953982562a9945b3fb0c8d18af557c4e64
M1kss <boytsovs.av@phystech.edu>
2019-12-06 05:07:51 +0300

    bias

--- scripts/FIGURES/visualize_bias.py
+++ scripts/FIGURES/visualize_bias.py
@@ -8,8 +8,9 @@ stats = pd.read_table('bias_statistics.tsv')
 
 fig, ax = plt.subplots(figsize=(10, 8))
 
-sns.scatterplot(x=stats['allele_reads'], y=np.log10(stats['ref']), color='blue', ax=ax)
-sns.scatterplot(x=stats['allele_reads'], y=np.log10(stats['alt']), color='green', ax=ax)
+#sns.scatterplot(x=stats['allele_reads'], y=np.log(stats['ref']), color='blue', ax=ax)
+#sns.scatterplot(x=stats['allele_reads'], y=np.log(stats['alt']), color='green', ax=ax)
+sns.scatterplot(x=stats['allele_reads'], y=(stats['ref']-stats['alt'])/np.sqrt(stats['alt']), ax=ax)
 
 plt.grid(True)
 plt.show()
--- scripts/QUALITYcontrol/CollectRefBiasStatistics.py
+++ scripts/QUALITYcontrol/CollectRefBiasStatistics.py
@@ -7,22 +7,25 @@ sys.path.insert(1, "/home/abramov/ASB-Project")
 from scripts.HELPERS.paths import cl_dict_path, parameters_path
 
 
-name = sys.argv[1]
+name = '*'
 
 with open(cl_dict_path, "r") as read_file:
     cell_lines_dict = json.loads(read_file.readline())
 sum_df = None
-for align_path in cell_lines_dict[name]:
-    if not os.path.isfile(align_path):
-        continue
-    print(align_path)
-    df = pd.read_table(align_path)
-    if df.empty:
-        continue
-    if sum_df is None:
-        sum_df = df[['ref_read_counts', 'alt_read_counts']]
-    else:
-        sum_df = sum_df.append(df[['ref_read_counts', 'alt_read_counts']])
+
+for key in cell_lines_dict:
+    for align_path in cell_lines_dict[key]:
+        if not os.path.isfile(align_path):
+            continue
+        print(align_path)
+        df = pd.read_table(align_path)
+        if df.empty:
+            continue
+        if sum_df is None:
+            sum_df = df[['ref_read_counts', 'alt_read_counts']]
+        else:
+            sum_df = sum_df.append(df[['ref_read_counts', 'alt_read_counts']])
+
 with open(parameters_path + 'bias_statistics.tsv', 'w') as out:
     out_t = pd.DataFrame()
     ser = sum_df['ref_read_counts'].value_counts()
