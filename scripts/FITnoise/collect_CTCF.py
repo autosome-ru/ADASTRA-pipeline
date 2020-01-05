57ac849532de41ee9597d4d5290bbc0d9dd6e728
wish1 <aswq22013@gmail.com>
2020-01-05 23:19:49 +0300

    collect CTCF

--- scripts/FITnoise/collect_CTCF.py
+++ scripts/FITnoise/collect_CTCF.py
@@ -56,39 +56,41 @@ def get_another_agr(path):
 
 
 if __name__ == '__main__':
-    key_name = "CTCF_HUMAN"
+    keys = ["CTCF_HUMAN"]
 
     with open(tf_dict_path, "r") as read_file:
         tf_dict = json.loads(read_file.readline())
     with open(cl_dict_path, "r") as read_file:
         cell_lines_dict = json.loads(read_file.readline())
         inv_cl_dict = invert(cell_lines_dict)
-    tables = list(map(lambda x: x.replace('table_p', 'table_BADs'), tf_dict[key_name]))
-
-    common_snps = []
-    for table in tables:
-        if os.path.isfile(table):
-            table_name = get_name(table)
-            with open(table, 'r') as file:
-                for line in file:
-                    if line[0] == "#":
-                        continue
-                    chr, pos, ID, ref, alt, ref_c, alt_c, repeat, _, _, _, _, ploidy, = line.strip().split("\t")[:13]
-                    if ploidy == "0":
-                        continue
-                    common_snps.append([chr, pos, ID, ref, alt, ref_c, alt_c, repeat, ploidy, table_name,
-                                        inv_cl_dict[table.replace('table_BADs', 'table_p')]])
-
-    print('Writing {}'.format(key_name))
-
-    with open(parameters_path + key_name + '_SNP_table.tsv', 'w') as out:
+    keys = tf_dict.keys()
+    with open(parameters_path + 'Union_SNP_table.tsv', 'w') as out:
         out.write(pack(['#chr', 'pos', 'ID', 'ref', 'alt', 'ref_read_counts', 'alt_read_counts', 'repeat',
                         'BAD', 'ALIGN_NAME', 'Cell_line']))
+        for key_name in keys:
+            tables = list(map(lambda x: x.replace('table_p', 'table_BADs'), tf_dict[key_name]))
 
-        common_snps = sorted(common_snps, key=lambda chr_pos: chr_pos[1])
-        common_snps = sorted(common_snps, key=lambda chr_pos: chr_pos[0])
-        BADs = set()
-        for snp in common_snps:
-            BADs.add(snp[-3])
-            out.write(pack(snp))
-    print(BADs)
+            common_snps = []
+            for table in tables:
+                if os.path.isfile(table):
+                    table_name = get_name(table)
+                    with open(table, 'r') as file:
+                        for line in file:
+                            if line[0] == "#":
+                                continue
+                            chr, pos, ID, ref, alt, ref_c, alt_c, repeat, _, _, _, _, ploidy, = line.strip().split("\t")[:13]
+                            if ploidy == "0":
+                                continue
+                            common_snps.append([chr, pos, ID, ref, alt, ref_c, alt_c, repeat, ploidy, table_name,
+                                                inv_cl_dict[table.replace('table_BADs', 'table_p')]])
+
+            print('Writing {}'.format(key_name))
+
+
+            common_snps = sorted(common_snps, key=lambda chr_pos: chr_pos[1])
+            common_snps = sorted(common_snps, key=lambda chr_pos: chr_pos[0])
+            BADs = set()
+            for snp in common_snps:
+                BADs.add(snp[-3])
+                out.write(pack(snp))
+        print(BADs)
