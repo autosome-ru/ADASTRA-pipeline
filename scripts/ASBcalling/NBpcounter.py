dc570ddbca462263fa7fe929337612532a9c84f2
M1kss <boytsovs.av@phystech.edu>
2020-01-06 05:21:24 +0300

    revert constrained BAD 1 1.5 2 3

--- scripts/ASBcalling/NBpcounter.py
+++ scripts/ASBcalling/NBpcounter.py
@@ -15,10 +15,10 @@ def count_p(ref_c, alt_c, BADs):
 
     for i in range(N):
 
-        if BADs[i] not in [1, 1.5, 2, 3]:  # TODO: DON'T FORGET TO FIX
-            p_ref[i] = '.'
-            p_alt[i] = '.'
-            continue
+        # if BADs[i] not in [1, 1.5, 2, 3]:  # TODO: DON'T FORGET TO FIX
+        #     p_ref[i] = '.'
+        #     p_alt[i] = '.'
+        #     continue
 
         if ref_c[i] > 500:
             r = ref_c[i]
