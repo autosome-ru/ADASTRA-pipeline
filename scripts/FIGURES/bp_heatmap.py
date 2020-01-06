2ceec6d07ac540a173fcc29e0ad20e3b1483c593
wish1 <aswq22013@gmail.com>
2020-01-06 14:30:50 +0300

    heatmap script fixes

--- scripts/FIGURES/bp_heatmap.py
+++ scripts/FIGURES/bp_heatmap.py
@@ -7,11 +7,13 @@ import matplotlib.pyplot as plt
 sns.set_style("white")
 sns.set_context({"figure.figsize": (10, 8)})
 
-df1 = pd.read_table('statistics_for_TFs_{}.tsv'.format('ref'))
-df1.index = df1.columns
+df1 = pd.read_table(os.path.expanduser('~/statistics_for_TFs_{}.tsv'.format('ref')))
+df1.index = df1.iloc[:, 0]
+df1 = df1.drop(df1.columns[0], 1)
 
-df2 = pd.read_table('statistics_for_TFs_{}.tsv'.format('alt'))
-df2.index = df2.columns
+df2 = pd.read_table(os.path.expanduser('~/statistics_for_TFs_{}.tsv'.format('alt')))
+df2.index = df2.iloc[:, 0]
+df2 = df2.drop(df2.columns[0], 1)
 
 df = df1 + df2
 df = df.astype(int)
@@ -22,5 +24,5 @@ plt.xlabel('Max cover tr')
 
 plt.grid(True)
 plt.title('All TFs, database size')
-plt.savefig('All_TFs_heat.png')
+plt.savefig(os.path.expanduser('~/All_TFs_heat.png'))
 plt.show()
