a55940f2b77385f78f050bd76567ca3802719af6
M1kss <boytsovs.av@phystech.edu>
2020-01-06 04:29:32 +0300

    NO RS - NO LIVING

--- scripts/ASBcalling/NBpcounter.py
+++ scripts/ASBcalling/NBpcounter.py
@@ -14,26 +14,30 @@ def count_p(ref_c, alt_c, BADs):
     p_alt = np.zeros(N, dtype=np.float128)
 
     for i in range(N):
-        if BADs[i] != 1:
-            p_ref[i] = 'NaN'
-            p_alt[i] = 'NaN'
-        else:
-            fixed = min(ref_c[i], alt_c[i])
-            main = max(ref_c[i], alt_c[i])
-            if ref_c[i] > alt_c[i]:
-                cdf = (st.nbinom(fixed - 1, 1 / (BADs[i] + 1))).cdf
-                pv = (1 - cdf(main)) / (1 - cdf(4))
-                p_alt[i] = 'NaN'
-                p_ref[i] = pv
-            elif ref_c[i] < alt_c[i]:
-                cdf = (st.nbinom(fixed - 2, 1 / (BADs[i] + 1))).cdf
-                pv = (1 - cdf(main)) / (1 - cdf(4))
-                p_alt[i] = pv
-                p_ref[i] = 'NaN'
-            else:
-                p_alt[i] = 'NaN'
-                p_ref[i] = 'NaN'
 
+        if ref_c[i] > 500:
+            p_alt[i] = 1
+        else:
+            r, w = weights[BADs[i]]['ref'][ref_c[i], :2]
+            if r == 0:
+                p_alt[i] = 1
+            else:
+                cdf1 = (st.nbinom(r, 1 / (BADs[i] + 1))).cdf
+                cdf2 = (st.nbinom(r, BADs[i] / (BADs[i] + 1))).cdf
+                cdf = lambda x: w * cdf1(x) + (1 - w) * cdf2(x)
+                p_alt[i] = (1 - cdf(alt_c[i])) / (1 - cdf(4))
+
+        if alt_c[i] > 500:
+            p_ref[i] = 1
+        else:
+            r, w = weights[BADs[i]]['alt'][alt_c[i], :2]
+            if r == 0:
+                p_ref[i] = 1
+            else:
+                cdf1 = (st.nbinom(r, 1 / (BADs[i] + 1))).cdf
+                cdf2 = (st.nbinom(r, BADs[i] / (BADs[i] + 1))).cdf
+                cdf = lambda x: w * cdf1(x) + (1 - w) * cdf2(x)
+                p_ref[i] = (1 - cdf(ref_c[i])) / (1 - cdf(4))
     return p_ref, p_alt
 
 
@@ -42,6 +46,12 @@ if __name__ == '__main__':
     table_BAD = full_path + "_table_BADs.txt"
     output = full_path + "_table_p.txt"
     print('Now counting P-value for {}'.format(table_BAD))
+
+    weights = {}
+    for BAD in states:
+        weights[BAD] = dict()
+        weights[BAD]['ref'] = np.load(parameters_path + 'NBweights_ref_BAD={:.1f}'.format(BAD))
+        weights[BAD]['alt'] = np.load(parameters_path + 'NBweights_alt_BAD={:.1f}'.format(BAD))
 
     df_with_BAD = pd.read_table(table_BAD)
     p_ref, p_alt = count_p(np.array(df_with_BAD["ref_read_counts"], dtype=np.int_),
--- scripts/FITnoise/fit_negative_binom_with_weights.py
+++ scripts/FITnoise/fit_negative_binom_with_weights.py
@@ -5,10 +5,11 @@ import pandas as pd
 from matplotlib import pyplot as plt, ticker
 from scipy import optimize
 from scipy import stats as st
-from scipy.special import beta, psi, poch, comb
-from sklearn import metrics
 import seaborn as sns
-import subprocess
+
+sys.path.insert(1, "/home/abramov/ASB-Project")
+from scripts.HELPERS.paths import parameters_path
+from scripts.HELPERS.helpers import states
 
 pd.set_option('display.max_columns', 7)
 
@@ -56,26 +57,27 @@ def make_scaled_counts(stats_pandas_dataframe):
 
     for index, row in stats_pandas_dataframe.iterrows():
         k, SNP_counts = row['{}_counts'.format(main_allele)], row['counts']
-        if main_allele == 'ref':
-            if k <= 4:
-                continue
-            k_raw = scaling[k]
-            k_floor = np.floor(k_raw)
-            k_ceil = np.ceil(k_raw)
-            part = k_raw - k_floor
-            floor_counts = np.ceil(SNP_counts * (1 - part))
-            ceil_counts = SNP_counts - floor_counts
-            nonzero_set.add(k_ceil)
-            nonzero_set.add(k_floor)
 
-            print(k_raw, k_floor, k_ceil, SNP_counts, floor_counts, ceil_counts, part)
-
-            # counts_array[int(k_floor)] += SNP_counts
-            counts_array[int(k_floor)] += floor_counts
-            counts_array[int(k_ceil)] += ceil_counts
-        else:
-            nonzero_set.add(k)
-            counts_array[k] += SNP_counts
+        # if main_allele == 'ref':
+        #     if k <= 4:
+        #         continue
+        #     k_raw = scaling[k]
+        #     k_floor = np.floor(k_raw)
+        #     k_ceil = np.ceil(k_raw)
+        #     part = k_raw - k_floor
+        #     floor_counts = np.ceil(SNP_counts * (1 - part))
+        #     ceil_counts = SNP_counts - floor_counts
+        #     nonzero_set.add(k_ceil)
+        #     nonzero_set.add(k_floor)
+        #
+        #     print(k_raw, k_floor, k_ceil, SNP_counts, floor_counts, ceil_counts, part)
+        #
+        #     # counts_array[int(k_floor)] += SNP_counts
+        #     counts_array[int(k_floor)] += floor_counts
+        #     counts_array[int(k_ceil)] += ceil_counts
+        # else:
+        nonzero_set.add(k)
+        counts_array[k] += SNP_counts
     return counts_array, nonzero_set
 
 
@@ -97,9 +99,10 @@ def plot_histogram(n, counts_array, plot_fit=None, save=True):
         w = plot_fit[1]
         current_density = make_negative_binom_density(r, get_p(), w, n, for_plot=True)
         cdf = lambda x: w * st.nbinom(r, get_p()).cdf(x) + (1 - w) * st.nbinom(fix_c, 1 - get_p()).cdf(x)
-        q_5 = min(x for x in range(number + 1) if cdf(x) >= 1 - 1 / (10 ** 5))
+        q_5_l = [x for x in range(number + 1) if cdf(x) >= 1 - 1 / (10 ** 5)]
+        q_5 = min(q_5_l) if q_5_l else False
 
-        asb = counts_array[q_5:].sum()
+        asb = counts_array[q_5:].sum() if q_5 else 0
         gof = calculate_gof(counts_array, w, r)
 
         label = 'negative binom fit for {}\ntotal observations: {}\nr={:.2f}, p={:.2f}, q={}, w={:.2f}\n' \
@@ -121,8 +124,8 @@ def fit_negative_binom(n, counts_array):
     #print("fit started")
     try:
         x = optimize.minimize(fun=make_log_likelihood(n, counts_array),
-                              x0=np.array([(fix_c if main_allele == 'alt' else scaling[fix_c], 0.5)]),
-
+                              #x0=np.array([(fix_c if main_allele == 'alt' else scaling[fix_c], 0.5)]),
+                              x0=np.array([fix_c, 0.5]),
                               bounds=[(0.00001, None), (0, 1)])
     except ValueError:
         return 'NaN', 10
@@ -179,75 +182,47 @@ def calculate_gof(counts_array, w, r):
 
 
 if __name__ == '__main__':
-    for main_allele in ("ref", ):
-        if main_allele in ('ref', 'alt'):
-            alleles = ('ref', 'alt')
-            other_allele = "ref" if main_allele == "alt" else "alt"
-        else:
-            alleles = ('min', 'max')
-            other_allele = "min" if main_allele == "max" else "max"
-        fix_c_array = [5, 8, 10, 12, 15, 17, 20, 23, 25, 28, 30]
+    for main_allele in ("alt", "ref"):
+        other_allele = "ref" if main_allele == "alt" else "alt"
+        alleles = ('ref', 'alt')
+
         fix_c_array = list(range(5, 501))
-        save_array = np.zeros((max(fix_c_array) + 1, 2), dtype=np.float_)
+        save_array = np.zeros((max(fix_c_array) + 1, 4), dtype=np.float_)
 
-        for BAD in [1]:
+        for BAD in states:
 
-            scale_df = pd.read_table(os.path.expanduser('~/ref_counts_scaling_BAD={:.1f}.tsv'.format(BAD)))
-            scaling = dict(zip(scale_df['allele_reads'], scale_df['new_allele_reads']))
+            #scale_df = pd.read_table(os.path.expanduser('~/ref_counts_scaling_BAD={:.1f}.tsv'.format(BAD)))
+            #scaling = dict(zip(scale_df['allele_reads'], scale_df['new_allele_reads']))
+
+            # filename = os.path.expanduser('~/cover_bias_statistics_norm_diploids.tsv'.format(BAD))
+            filename = parameters_path + 'fixed_alt_bias_statistics_BAD={:.1f}.tsv'.format(BAD)
+            stats = pd.read_table(filename)
+            for allele in alleles:
+                stats['{}_counts'.format(allele)] = stats['{}_counts'.format(allele)].astype(int)
 
             for fix_c in fix_c_array:
-                # filename = os.path.expanduser('~/cover_bias_statistics_norm_diploids.tsv'.format(BAD))
-                filename = os.path.expanduser('~/fixed_alt_bias_statistics_BAD={:.1f}.tsv'.format(BAD))
-                stats = pd.read_table(filename)
-                for allele in alleles:
-                    stats['{}_counts'.format(allele)] = stats['{}_counts'.format(allele)].astype(int)
-                if main_allele == 'alt':
-                    stats = stats[stats['ref_counts'] == fix_c]
-                else:
-                    stats = stats[stats['alt_counts'] == fix_c]
-                # counts, dict_of_nonzero_N = make_counts_array_and_nonzero_set(stats)
+                stats = stats[stats['alt_counts'] == fix_c]
                 counts, set_of_nonzero_n = make_scaled_counts(stats)
 
-                if len(set_of_nonzero_n) == 0 or counts.sum() < max(set_of_nonzero_n):
+                if len(set_of_nonzero_n) == 0 or counts.sum() < max(set_of_nonzero_n) - 5:
                     continue
                 print('made counts')
                 print('Fix {}={}'.format(other_allele, fix_c))
                 number = len(counts) - 1
 
-                #number = min(len(counts) - 1, 250)
+                left_most = 5
+                q_left = 5
+                right_most = len(counts) - 1
 
-                if BAD == 6:
-                    t_left = 10
-                else:
-                    t_left = 15
-                t_right = 100
-
-                total_snps = counts.sum()
-                cdf = counts.cumsum() / total_snps
-                assert cdf[-1] == 1
-                q_left = min(x for x in range(len(counts)) if cdf[x] >= t_left / 100)
-                q_right = max(x for x in range(len(counts)) if cdf[x] <= t_right / 100)
-
-                # print('q={} {}'.format(q_left, q_right))
-                # left_most = max(5, q_left)
-                right_most = len(counts)
-
-                q_right = min(q_right, number)
 
                 calculate_negative_binom = True
                 # weights = (fix_c, 0.5)
                 # plot_histogram(number, counts, plot_fit=weights)
                 if calculate_negative_binom:
-                    wg_list = []
-                    for q_left in range(5, fix_c + 5):
-                        left_most = max(5, q_left)
-                        weights, gof = fit_negative_binom(right_most, counts)
-                        wg_list.append((weights, gof, q_left))
+                    weights, gof = fit_negative_binom(right_most, counts)
+                    # plot_histogram(number, counts, plot_fit=weights.x)
+                    save_array[fix_c, :2] = weights.x
+                    save_array[fix_c, 2] = weights.success
+                    save_array[fix_c, 3] = gof
 
-                    weights, gof, q_left = min(wg_list, key=lambda x: x[1])
-                    left_most = max(5, q_left)
-                    print('q={} {}'.format(q_left, q_right))
-                    plot_histogram(number, counts, plot_fit=weights.x)
-                    save_array[fix_c, :] = weights.x
-
-            np.save(os.path.expanduser("~/NB_fit_BAD_{}={:.1f}.npy".format(main_allele, BAD)), save_array)
+            np.save(os.path.expanduser(parameters_path + 'NBweights_{}_BAD={:.1f}'.format(other_allele, BAD)), save_array)
