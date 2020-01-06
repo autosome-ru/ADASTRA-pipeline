2808865753af70251b714c436d0aa8af8a3728c4
wish1 <aswq22013@gmail.com>
2020-01-06 15:00:01 +0300

    save_weights_to_df.py

---
+++ scripts/QUALITYcontrol/save_weights_to_df.py
@@ -0,0 +1,20 @@
+import sys
+import numpy as np
+import pandas as pd
+
+sys.path.insert(1, "/home/abramov/ASB-Project")
+from scripts.HELPERS.paths import parameters_path
+from scripts.HELPERS.helpers import states
+
+
+column_names = ['r', 'w', 'status', 'gof']
+for BAD in states:
+    ref = np.load(parameters_path + 'NBweights_ref_BAD={:.1f}'.format(BAD))
+    alt = np.load(parameters_path + 'NBweights_alt_BAD={:.1f}'.format(BAD))
+    ref_df = pd.DataFrame(columns=column_names)
+    alt_df = pd.DataFrame(columns=column_names)
+    for i in range(len(column_names)):
+        ref_df[column_names[i]] = ref[i]
+        alt_df[column_names[i]] = alt[i]
+    ref_df.to_csv(parameters_path + 'weights/NBweights_ref_BAD={:.1f}'.format(BAD), sep='\t', index=False)
+    alt_df.to_csv(parameters_path + 'weights/NBweights_ref_BAD={:.1f}'.format(BAD), sep='\t', index=False)
