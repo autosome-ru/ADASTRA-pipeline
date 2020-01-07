4483a33cfd2478bf4ce0c957304116a424a0d368
wish1 <aswq22013@gmail.com>
2020-01-07 15:07:30 +0300

    removed makedensity from pipeline_start.sh

--- scripts/pipeline_start.sh
+++ scripts/pipeline_start.sh
@@ -17,7 +17,6 @@ if [ "$flag" == --BAD ]; then
 fi
 
 if [ "$flag" == --pvalue ] || [ "$flag" == --BAD ]; then
-  python3 "$ScriptsFolder"ASBcalling/MakeDensity.py
   bash "$ScriptsFolder"PARAMETERS/p_value_count.sh "$njobs"
 fi
 
