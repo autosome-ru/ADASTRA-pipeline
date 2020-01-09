3a641e25f2c17dfdde790d18d48ff97b967bea4f
wish1 <aswq22013@gmail.com>
2020-01-09 15:19:47 +0300

    agr

--- scripts/ASBcalling/post_process.py
+++ scripts/ASBcalling/post_process.py
@@ -18,7 +18,17 @@ print("Counting FDR for {} {}".format(what_for, key_name))
 table = pd.read_table(results_path + what_for + "_P-values/" + key_name + '_common_table.tsv')
 if table.empty:
     sys.exit(0)
+old_table = pd.read_table(results_path + 'Processed/' + what_for + "_P-values/" + key_name + '_common_table.tsv')
 
+table[['refc_maxdepth', 'altc_maxdepth',
+       'BAD_maxdepth', 'm_maxdepth',
+       'refc_mostsig', 'altc_mostsig',
+       'BAD_mostsig', 'm_mostsig',
+       'm_mean_ref', 'm_mean_alt']] = old_table[['refc_maxdepth', 'altc_maxdepth',
+                                                 'BAD_maxdepth', 'm_maxdepth',
+                                                 'refc_mostsig', 'altc_mostsig',
+                                                'BAD_mostsig', 'm_mostsig',
+                                                 'm_mean_ref', 'm_mean_alt']]
 bool_ar_ref, p_val_ref, _, _ = statsmodels.stats.multitest.multipletests(table["logitp_ref"],
                                                                          alpha=0.05, method='fdr_by')
 bool_ar_alt, p_val_alt, _, _ = statsmodels.stats.multitest.multipletests(table["logitp_alt"],
@@ -27,5 +37,5 @@ table["fdrp_by_ref"] = pd.Series(p_val_ref)
 table["fdrp_by_alt"] = pd.Series(p_val_alt)
 
 
-with open(results_path + 'Processed/' + what_for + "_P-values/" + key_name + '_common_table.tsv', "w") as w:
+with open(results_path + 'ProcessedNew/' + what_for + "_P-values/" + key_name + '_common_table.tsv', "w") as w:
     table.to_csv(w, sep="\t", index=False)
