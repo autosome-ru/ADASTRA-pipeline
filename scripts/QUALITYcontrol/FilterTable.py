1bc37f791bf97c2c01bb8c105fbecac9d186f479
wish1 <aswq22013@gmail.com>
2020-01-09 13:18:17 +0300

    debug

--- scripts/QUALITYcontrol/FilterTable.py
+++ scripts/QUALITYcontrol/FilterTable.py
@@ -24,7 +24,7 @@ def filterTable(table, mc_tr=10, totc_tr=10, alt=False):
 def filterTableNAgg(table, mc_tr=10, n=0, alt=False):
     table = table[(table["max_cover"] >= mc_tr) & (table["n_aggregated"] >= n)]
     if table.empty:
-        return 0
+        return 0, 0
     if alt:
         bool_ar, p_val, _, _ = statsmodels.stats.multitest.multipletests(table["logitp_alt"], alpha=0.01,
                                                                          method='fdr_by')
