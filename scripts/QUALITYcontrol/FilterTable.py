45d53386c88bf285b777ebbcfcc8e271808b048c
wish1 <aswq22013@gmail.com>
2020-01-09 16:09:34 +0300

    agr

--- scripts/QUALITYcontrol/FilterTable.py
+++ scripts/QUALITYcontrol/FilterTable.py
@@ -26,10 +26,10 @@ def filterTableNAgg(table, mc_tr=10, n=0, alt=False):
     if table.empty:
         return 0, 0
     if alt:
-        bool_ar, p_val, _, _ = statsmodels.stats.multitest.multipletests(table["logitp_alt"], alpha=0.01,
+        bool_ar, p_val, _, _ = statsmodels.stats.multitest.multipletests(table["logitp_alt"], alpha=0.05,
                                                                          method='fdr_by')
     else:
-        bool_ar, p_val, _, _ = statsmodels.stats.multitest.multipletests(table["logitp_ref"], alpha=0.01,
+        bool_ar, p_val, _, _ = statsmodels.stats.multitest.multipletests(table["logitp_ref"], alpha=0.05,
                                                                          method='fdr_by')
 
     return sum(bool_ar), len(table.index)
