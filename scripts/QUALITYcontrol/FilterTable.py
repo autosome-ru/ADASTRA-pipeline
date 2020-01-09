0682981852327fe27e23f66b2ad7d62aad56358d
wish1 <aswq22013@gmail.com>
2020-01-09 06:28:38 +0300

    debug

--- scripts/QUALITYcontrol/FilterTable.py
+++ scripts/QUALITYcontrol/FilterTable.py
@@ -36,7 +36,7 @@ def filterTableNAgg(table, mc_tr=10, n=0, alt=False):
 
 flag_d = {"totc": "_total_cov", "mc": "_max_cov"}
 mc_list = list(range(10, 61, 10))
-totc_list = list(range(1, 3))
+totc_list = list(range(1, 2))
 columns_list = [str(i) for i in mc_list]
 table = pd.DataFrame(columns=columns_list)
 
