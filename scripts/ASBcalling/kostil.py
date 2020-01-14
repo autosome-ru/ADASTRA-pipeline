001b832c16a8abe5ae128b43e772c38f82111b71
wish1 <aswq22013@gmail.com>
2020-01-14 03:11:08 +0300

    kostil added

--- scripts/ASBcalling/kostil.py
+++ scripts/ASBcalling/kostil.py
@@ -28,7 +28,7 @@ def makedict():
             continue
         ln = line.split("\t")
         cell_line = encode_GTRD_cell_line_name(ln[4])
-        d[cell_line] = remove_punctuation(ln[4])
+        d[cell_line] = remove_punctuation(ln[4]).replace(" ", "_")
     return d
 
 
@@ -55,7 +55,7 @@ for file in os.listdir(results_path + what_for + '_P-values/'):
         cell_line = convert_cl[file.replace("_common_table.tsv", "")]
         new_table_path = results_path + "FORGTRD/" + what_for + '_P-values/{}.tsv'.format(cell_line)
     else:
-        new_table_path = results_path + "FORGTRD/" + what_for + '_P-values/{}'.format(file.replace("_common_table", ""))
+        new_table_path = results_path + "FORGTRD/" + what_for + '_P-values/{}'.format(file[:-1 * len("_common_table")])
     print(file, new_table_path)
     table.to_csv(new_table_path, sep="\t", index=False)
 
@@ -63,7 +63,7 @@ for file in os.listdir(results_path + what_for + '_DICTS/'):
 
     dict_path = results_path + what_for + '_DICTS/{}'.format(file)
     if what_for == "CL":
-        new_name = convert_cl[file.replace('_DICT.json', '')] + '_DICT.json'
+        new_name = convert_cl[file[:-1*len('_DICT.json')]] + '_DICT.json'
     else:
         new_name = file
     new_dict_path = results_path + "FORGTRD/" + what_for + '_DICTS/{}'.format(new_name)
