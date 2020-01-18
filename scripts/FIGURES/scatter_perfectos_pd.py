d365da3e3937e5a8720c30ce0128bf7a6b0e27ae
wish1 <aswq22013@gmail.com>
2020-01-18 05:10:54 +0300

    SEREGA commit with filter in get color

--- scripts/FIGURES/scatter_perfectos_pd.py
+++ scripts/FIGURES/scatter_perfectos_pd.py
@@ -1,40 +1,44 @@
 import sys
 import numpy as np
+import os
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 
 
-def get_color(pv, fc):
-    if abs(fc) < fc_tr or abs(pv) < fdr_tr:
+def get_color(row):
+    if abs(row['log_fc']) < np.log10(fc_tr) or \
+            (abs(row['fdrp_by_ref']) > fdr_tr and abs(row['fdrp_by_alt']) > fdr_tr)\
+            or (row['motif_log_pref'] < -np.log10(perf_tr)) or (row['motif_log_palt'] < -np.log10(perf_tr)):
         return 'grey'
-    if fc * pv > 0:
+    if row['log_fc'] * row['log_pv'] > 0:
         return 'blue'
     else:
         return 'red'
 
 
 if __name__ == '__main__':
-    pt = pd.read_table(sys.argv[1])
+    filepath = os.path.expanduser("~/CTCF_HUMAN_fc.tsv")
+    pt = pd.read_table(filepath)
 
-    perf_tr = 1.3
-    fc_tr = np.log10(2)
-    fdr_tr = -np.log10(0.05)
+    perf_tr = 0.00005
+    fc_tr = 2
+    fdr_tr = 0.05
     fix = ''
 
-    pt = pt[(pt['motif_log_pref'] >= perf_tr) & (pt['motif_log_palt'] >= perf_tr)]
-
+    pt = pt[~(pt['fdrp_by_alt'].isnull() | pt['fdrp_by_ref'].isnull())]
+    # pt = pt[(pt['fdrp_by_alt'] <= fdr_tr) | (pt['fdrp_by_ref'] <= fdr_tr)]
+    print(pt)
+    pt['log_pv'] = (np.log10(
+        pt['fdrp_by_ref']) - np.log10(pt['fdrp_by_alt']))
     pt['log_fc'] = pt['fold_change']
-    pt['log_pv'] = np.sign(pt['fdrp_by_alt' + fix] - pt['fdrp_by_ref' + fix]) * (np.log10(
-        pt['fdrp_by_alt']) - np.log10(pt['fdrp_by_ref']))
-    pt['col'] = pt.apply(lambda x: get_color(x['log_pv'],
-                                             x['log_fc']), axis=1)
+    pt['col'] = pt.apply(lambda x: get_color(x), axis=1)
 
     print(pt.info())
 
-    blue = len(pt[pt.col == get_color(1000, 1000)].index)
-    red = len(pt[pt.col == get_color(1000, -1000)].index)
-    grey = len(pt[pt.col == get_color(0, 0)].index)
+    blue = len(pt[pt.col == 'blue'].index)
+    red = len(pt[pt.col == 'red'].index)
+    grey = len(pt[pt.col == 'grey'].index)
 
     print(pt['log_pv'])
 
@@ -42,7 +46,7 @@ if __name__ == '__main__':
     plt.scatter(x=list(pt['log_pv']), y=list(pt['log_fc']), c=pt['col'], s=5)
     plt.grid(True)
     plt.title(
-        '\n'.join(['perfectos_pv <= {}', 'fold_change >= {}', 'fdr <= {}', fix]).format(perf_tr, 10 ** fc_tr, round(10 ** (-fdr_tr), 2)))
+        '\n'.join(['perfectos_pv <= {}', 'fold_change >= {}', 'fdr <= {}', fix]).format(perf_tr, fc_tr, fdr_tr))
     plt.xlabel('signed -log10 fdr_p')
     plt.ylabel('log10 prefectos foldchange')
     label = 'blue/red: {0}/{1}({2}%),\ngrey/all={3}%'.format(blue, red,
@@ -52,4 +56,5 @@ if __name__ == '__main__':
     plt.text(x=min(pt.log_pv) * 4 / 5, y=min(pt.log_fc) / 2,
              s=' log P-value treshold: {},\nlog FC treshold: {}'.format(round(fdr_tr, 1), round(fc_tr, 1)))
 
-    plt.savefig('_'.join(map(str, [sys.argv[1].split('/')[-1], perf_tr, fc_tr, fdr_tr, fix])) + '.png')
+    plt.savefig(os.path.expanduser("~/{}_p_tr={:.2f}_fc_tr={:.2f}_fdr_tr={:.2f}.png".format(filepath.split('/')[-1],
+                                                                                perf_tr, fc_tr, fdr_tr)))
