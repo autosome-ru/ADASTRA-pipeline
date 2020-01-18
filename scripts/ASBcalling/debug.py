a2c542b555b6dc9b731eef7f0cbdefc9bbcfac8e
wish1 <aswq22013@gmail.com>
2020-01-19 01:18:04 +0300

    debug

--- scripts/ASBcalling/debug.py
+++ scripts/ASBcalling/debug.py
@@ -193,7 +193,8 @@ if __name__ == '__main__':
                                                 debug_dict[tr][x]['p'] <= strange_tr)])]
         x = list(strange_list)
         print([final_dict[tr][x_] for x_ in x])
-        y1, y2 = zip([final_dict[tr][x_] for x_ in x])
+        y1 = [final_dict[tr][x_][0] for x_ in x]
+        y2 = [final_dict[tr][x_][1] for x_ in x]
         final_dict[tr] = {}
         final_dict[tr]['x'] = x
         final_dict[tr]['y1'] = y1
