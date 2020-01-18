36d4bf6a075a9b6d4db383310b9c00fbd87c2b3c
wish1 <aswq22013@gmail.com>
2020-01-18 18:21:38 +0300

    debug done

--- scripts/aggregation.sh
+++ scripts/aggregation.sh
@@ -7,9 +7,9 @@ indicator=$2
 
 if [ "$indicator" == "--forCL" ];then
   python3 "$scripts_path"PARAMETERS/MakeParametersForAgr.py "CL"
-  parallel --jobs "$njobs" python3 "$scripts_path"ASBcalling/Aggregation.py "CL" :::: "$parameters_path"/Agr_parameters.cfg
+  parallel --jobs "$njobs" python3 "$scripts_path"ASBcalling/Aggregation.py "CL" :::: "$parallel_parameters_path"Agr_parameters.cfg
 fi
 if [ "$indicator" == "--forTF" ];then
   python3 "$scripts_path"PARAMETERS/MakeParametersForAgr.py "TF"
-  parallel --jobs "$njobs" python3 "$scripts_path"ASBcalling/Aggregation.py "TF" :::: "$parameters_path"/Agr_parameters.cfg
+  parallel --jobs "$njobs" python3 "$scripts_path"ASBcalling/Aggregation.py "TF" :::: "$parallel_parameters_path"Agr_parameters.cfg
 fi
