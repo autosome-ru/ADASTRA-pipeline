5dd8cedc45ea8521e5a7a8a9e0b300bc40d678cd
wish1 <aswq22013@gmail.com>
2020-01-19 17:47:26 +0300

    debug

--- scripts/ASBcalling/debug.py
+++ scripts/ASBcalling/debug.py
@@ -175,7 +175,7 @@ if __name__ == '__main__':
             for v in value:
                 cov, ref_c, alt_c, in_callers, BAD, dip_qual, lq, rq, seg_c, sum_cov, p_ref, p_alt = v
 
-                if min(p_ref, p_alt) < 1/10**10 or min(ref_c, alt_c) > tr:
+                if min(p_ref, p_alt) < 1/10**6 or min(ref_c, alt_c) > tr:
                     continue
                 pref_array.append(p_ref)
                 palt_array.append(p_alt)
