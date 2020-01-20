fdb21cc72296d3f7281ecab767ed65d6f854d05e
wish1 <aswq22013@gmail.com>
2020-01-20 04:45:45 +0300

    debug

--- scripts/p_value_count.sh
+++ scripts/p_value_count.sh
@@ -4,4 +4,4 @@ source HELPERS/paths_for_components.py
 
 njobs=$1
 python3 "$scripts_path"PARAMETERS/MakeParametersForPvC.py
-parallel --jobs "$njobs" python3 "$scripts_path"ASBcalling/NBpcounter.py :::: "$parameters_path"/PvC_parameters.cfg
+parallel --jobs "$njobs" python3 "$scripts_path"ASBcalling/NBpcounter.py :::: "$parallel_parameters_path"/PvC_parameters.cfg
