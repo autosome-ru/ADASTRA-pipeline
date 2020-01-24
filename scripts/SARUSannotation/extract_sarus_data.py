25de47e1a0bb4ce4a41b27b3feef60714f9846b7
wish1 <aswq22013@gmail.com>
2020-01-24 21:10:43 +0300

    fiiixes

--- scripts/SARUSannotation/extract_sarus_data.py
+++ scripts/SARUSannotation/extract_sarus_data.py
@@ -145,7 +145,8 @@ for line in table:
 
     if R.lower() != nuc[gen[chr][p]]:
         continue
-    print([gen[chr][p - (motive_length - 1) + i] for i in range(motive_length - 1)])
+    if 0 in [gen[chr][p - (motive_length - 1) + i] for i in range(motive_length - 1)]:
+        continue
     left_tail = ''.join([nuc[gen[chr][p - (motive_length - 1) + i]] for i in range((motive_length - 1))])
     right_tail = ''.join([nuc[gen[chr][p + 1 + i]] for i in range((motive_length - 1))])
 
