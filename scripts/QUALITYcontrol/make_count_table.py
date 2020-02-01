24c3b3c92514dc7430c12b19b1967d153a682983
wish1 <aswq22013@gmail.com>
2020-02-01 23:28:18 +0300

    cover to heatmap data

--- scripts/QUALITYcontrol/make_count_table.py
+++ scripts/QUALITYcontrol/make_count_table.py
@@ -8,4 +8,5 @@ for threshold in thresholds:
     df = df[df['cov'] > threshold]
     df_counts = df.groupby(['BAD', 'COSMIC']).size().reset_index(name='counts')
     df_counts = df_counts.groupby(['BAD', 'COSMIC'], as_index=False)['counts'].sum()
-    df_counts.to_csv(os.path.expanduser('~/counts_covm_{}.tsv'.format(threshold)), index=False, sep='\t')
+    df_counts.to_csv(os.path.expanduser(
+        '~/PARAMETERS/counts/counts_covm_{}.tsv'.format(threshold)), index=False, sep='\t')
