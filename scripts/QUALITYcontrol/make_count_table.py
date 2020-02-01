cb6e1c71a06b8cef1bae53090e2c056272bd7185
wish1 <aswq22013@gmail.com>
2020-02-01 23:30:52 +0300

    collect count table

--- scripts/QUALITYcontrol/make_count_table.py
+++ scripts/QUALITYcontrol/make_count_table.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import os
 
-df = pd.read_table(os.path.expanduser('~/unionSNPs.tsv'))
+df = pd.read_table(os.path.expanduser('~/uniuonSNPs.tsv'))
 df.columns = ['chr', 'pos', 'cov', 'BAD', 'COSMIC']
 thresholds = [x for x in range(10, 51, 5)]
 for threshold in thresholds:
+    print('Now doing threshold = {}'.format(threshold))
     df = df[df['cov'] > threshold]
     df_counts = df.groupby(['BAD', 'COSMIC']).size().reset_index(name='counts')
     df_counts = df_counts.groupby(['BAD', 'COSMIC'], as_index=False)['counts'].sum()
