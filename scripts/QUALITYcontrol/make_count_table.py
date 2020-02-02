5a0516777794bbfae045280ea90d615c7a2749d5
M1kss <boytsovs.av@phystech.edu>
2020-02-02 21:09:18 +0300

    ASWP mean qual

--- scripts/QUALITYcontrol/make_count_table.py
+++ scripts/QUALITYcontrol/make_count_table.py
@@ -3,12 +3,12 @@ import os
 
 df = pd.read_table(os.path.expanduser('~/uniuonSNPs.tsv'))
 df.columns = ['chr', 'pos', 'cov', 'BAD', 'COSMIC', 'quality']
-thresholds = [x for x in range(10, 101, 1)]
+thresholds = [x for x in range(101)] + [x for x in range(100, 1001, 10)]
 print(df['quality'].unique())
 sum_df = None
 for threshold in thresholds:
     print('Now doing threshold = {}'.format(threshold))
-    df = df[df['quality'] > threshold]
+    df = df[df['quality'] >= threshold]
     df_counts = df.groupby(['BAD', 'COSMIC']).size().reset_index(name='counts')
     df_counts = df_counts.groupby(['BAD', 'COSMIC'], as_index=False)['counts'].sum()
     df_counts['threshold'] = threshold
