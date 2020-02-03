bd46888b17b24dc040b39bc1350d1f09a3ca840d
M1kss <boytsovs.av@phystech.edu>
2020-02-04 02:14:07 +0300

    fix

--- scripts/QUALITYcontrol/make_count_table.py
+++ scripts/QUALITYcontrol/make_count_table.py
@@ -9,6 +9,7 @@ from scripts.HELPERS.helpers import states
 for BAD in states:
     df = pd.read_table(os.path.expanduser('~/unionSNPs.tsv'))
     df.columns = ['chr', 'pos', 'cov', 'BAD', 'COSMIC'] + ['Q{:.2f}'.format(state) for state in states]
+    df['BAD'] = BAD
     df['deltaQ{:.2f}'.format(BAD)] = df['Q{:.2f}'.format(BAD)] - df[
         ['Q{:.2f}'.format(another_BAD) for another_BAD in states if another_BAD != BAD]].max(axis=1)
     print(df['deltaQ{:.2f}'.format(BAD)].unique())
