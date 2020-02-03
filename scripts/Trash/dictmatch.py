e448d533980c08c8e16101b59d41815653842844
wish1 <aswq22013@gmail.com>
2020-02-03 17:47:10 +0300

    new remove punctuation

--- scripts/Trash/dictmatch.py
+++ scripts/Trash/dictmatch.py
@@ -8,7 +8,10 @@ with open(os.path.expanduser('~/PARAMETERS/CELL_LINES.json')) as f1, open(os.pat
 new_d = {}
 
 for key_old, value_old in old_d.items():
+    if "+" in key_old:
+        print(key_old)
     for key, value in d.items():
+
         if value == value_old:
             new_d[key_old] = key
             break
