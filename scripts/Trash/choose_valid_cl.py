8fc76f1cdb7c60e4c0a550bb972599a84adf6534
wish1 <aswq22013@gmail.com>
2020-02-05 19:02:09 +0300

    debug

--- scripts/Trash/choose_valid_cl.py
+++ scripts/Trash/choose_valid_cl.py
@@ -2,10 +2,10 @@ import json
 import os
 import pandas as pd
 
-inp_path = os.path.expanduser("~/DATA/CL_P-values/")
+inp_path = os.path.expanduser("~/DATA/TF_P-values/")
 
 for file in os.listdir(inp_path):
     table = pd.read_table(inp_path + file)
     if table.empty:
         print(inp_path + file)
-        os.remove(inp_path + file)
+        #os.remove(inp_path + file)
