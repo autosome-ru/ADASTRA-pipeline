d9bafcf753395a9096916e1f3e201e6abce22161
wish1 <aswq22013@gmail.com>
2020-02-05 18:55:52 +0300

    debug

--- scripts/Trash/choose_valid_cl.py
+++ scripts/Trash/choose_valid_cl.py
@@ -1,14 +1,12 @@
+import json
 import os
 
 inp_path = os.path.expanduser("~/DATA/CL_DICTS/")
-out_path = os.path.expanduser("~/DATA/CL_DICTS_new/")
-good_path = os.path.expanduser("~/ParallelParameters/Agr_parameters.cfg")
-good_one = set()
-with open(good_path) as f:
-    for line in f:
-        good_one.add(line.strip("\n"))
 
 for file in os.listdir(inp_path):
     print(file)
-    if file.replace("_DICT.json", "") in good_one:
-        os.rename(inp_path + file, out_path + file)
+    with open(inp_path + file) as f:
+        d = json.loads(f.readline())
+    if len(d.keys()) == 0:
+        print(inp_path + file)
+        #os.remove(inp_path + file)
