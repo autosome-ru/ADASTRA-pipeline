d0a2a2f60891769ed5eb8d41031c009e7048a0b0
wish1 <aswq22013@gmail.com>
2020-02-07 23:40:51 +0300

    neg binom plot

--- scripts/FIGURES/AS_figure_9_chips.py
+++ scripts/FIGURES/AS_figure_9_chips.py
@@ -49,56 +49,65 @@ df['color'] = df.apply(get_color, axis=1)
 df['hue'] = df.apply(get_hue, axis=1)
 df = df.sort_values('color', axis=0)
 df = df.dropna(subset=["cor_by_snp_CAIC"])
-# Draw barplot
-fig, ax = plt.subplots()
-
-fig.tight_layout(pad=1.5)
-# ax.margins(x=0, y=0)
 df = df.sort_values("cor_by_snp_CAIC", axis=0, ascending=False)
-
 df["delta_tau"] = df["cor_by_snp_CAIC"] - df["cor_by_snp_probe_CGH"]
-
-### FIXME для внимания ONLY FOR delta tau with chip !!!!!
-df = df.sort_values("delta_tau", axis=0, ascending=False)
-df = df.dropna(subset=["cor_by_snp_probe_CGH"])
-###
 
 df_k562 = df.apply(lambda x: get_cl(x, "K562"), axis=1)
 df_mcf7 = df.apply(lambda x: get_cl(x, "MCF7"), axis=1)
 df_other = df.apply(lambda x: get_cl(x, "other"), axis=1)
 
 # Draw 3 colors for cell lines vs cosmic
+fig, ax = plt.subplots()
+fig.tight_layout(pad=1.5)
+# ax.margins(x=0, y=0)
 
-# plt.stackplot(range(len(df.index)), df_k562["cor_by_snp_CAIC"], alpha=0.7,
-#               linewidth=0.7, color='C1', labels=["K562"])
-# plt.stackplot(range(len(df.index)), df_mcf7["cor_by_snp_CAIC"], alpha=0.7,
-#               linewidth=0.7, color='C2', labels=["MCF7"])
-# plt.stackplot(range(len(df.index)), df_other["cor_by_snp_CAIC"], alpha=0.7,
-#               linewidth=0.7, color='C0', labels=["other"])
+ax.stackplot(range(len(df.index)), df_k562["cor_by_snp_CAIC"], alpha=0.7,
+             linewidth=0.7, color='C1', labels=["K562"])
+ax.stackplot(range(len(df.index)), df_mcf7["cor_by_snp_CAIC"], alpha=0.7,
+             linewidth=0.7, color='C2', labels=["MCF7"])
+ax.stackplot(range(len(df.index)), df_other["cor_by_snp_CAIC"], alpha=0.7,
+             linewidth=0.7, color='C0', labels=["other"])
 
-# Draw delta tau with chip 3 cell lines
-
-plt.stackplot(range(len(df.index)), df_k562["delta_tau"], alpha=0.7,
-              linewidth=0.7, color='C1', labels=["K562"])
-plt.stackplot(range(len(df.index)), df_mcf7["delta_tau"], alpha=0.7,
-              linewidth=0.7, color='C2', labels=["MCF7"])
-plt.stackplot(range(len(df.index)), df_other["delta_tau"], alpha=0.7,
-              linewidth=0.7, color='C0', labels=["other"])
+ax.grid(True)
+ax.legend()
+plt.savefig(os.path.expanduser("~/AC_9/AS_Figure_9_cor_cosmic.png"), dpi=300)
+plt.close(fig)
 
 # Draw scatter vs COSMIC
+fig, ax = plt.subplots()
+fig.tight_layout(pad=1.5)
 
-# sns.scatterplot(x="cor_by_snp_CAIC", y="cor_by_snp_probe_CGH", hue='hue', data=df,  linewidth=0, alpha=0.5)
-# sns.lineplot(x=[-1, 1], y=[-1, 1], color='#505050')
-# plt.axvline(x=0, ymax=0, color='#505050', linestyle='--')
-# plt.axhline(y=0, xmax=0, color='#505050', linestyle='--')
-# plt.grid(True)
-# plt.xlim(-1, 1)
-# plt.ylim(-1, 1)
-
-plt.grid(True)
-plt.legend()
-# plt.show()
+sns.scatterplot(x="cor_by_snp_CAIC", y="cor_by_snp_probe_CGH", hue='hue', data=df, linewidth=0, alpha=0.5)
+sns.lineplot(x=[-1, 1], y=[-1, 1], color='#505050')
+ax.axvline(x=0, ymax=0, color='#505050', linestyle='--')
+ax.axhline(y=0, xmax=0, color='#505050', linestyle='--')
+ax.xlim(-1, 1)
+ax.ylim(-1, 1)
+ax.grid(True)
+plt.savefig(os.path.expanduser("~/AC_9/AS_Figure_9_scatter.png"), dpi=300)
+plt.close(fig)
 
 
+# delta tau 3 colors
+fig, ax = plt.subplots()
+fig.tight_layout(pad=1.5)
+
+df = df.sort_values("delta_tau", axis=0, ascending=False)
+df = df.dropna(subset=["cor_by_snp_probe_CGH"])
+
+df_k562 = df.apply(lambda x: get_cl(x, "K562"), axis=1)
+df_mcf7 = df.apply(lambda x: get_cl(x, "MCF7"), axis=1)
+df_other = df.apply(lambda x: get_cl(x, "other"), axis=1)
+
+ax.stackplot(range(len(df.index)), df_k562["delta_tau"], alpha=0.7,
+              linewidth=0.7, color='C1', labels=["K562"])
+ax.stackplot(range(len(df.index)), df_mcf7["delta_tau"], alpha=0.7,
+              linewidth=0.7, color='C2', labels=["MCF7"])
+ax.stackplot(range(len(df.index)), df_other["delta_tau"], alpha=0.7,
+              linewidth=0.7, color='C0', labels=["other"])
+
+ax.grid(True)
+ax.legend()
 plt.savefig(os.path.expanduser("~/AC_9/AS_Figure_9_delta_tau_chips.png"), dpi=300)
-plt.close()
+plt.close(fig)
+
