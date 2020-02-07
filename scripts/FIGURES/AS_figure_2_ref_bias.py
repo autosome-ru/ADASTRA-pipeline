c0061aabda9af3584555743e556d46554652a0e1
M1kss <boytsovs.av@phystech.edu>
2020-02-08 00:51:45 +0300

    fig2 changes

--- scripts/FIGURES/AS_figure_2_ref_bias.py
+++ scripts/FIGURES/AS_figure_2_ref_bias.py
@@ -12,7 +12,7 @@ plt.rcParams['font.weight'] = "medium"
 plt.rcParams['axes.labelweight'] = 'medium'
 plt.rcParams['figure.titleweight'] = 'medium'
 plt.rcParams['axes.titleweight'] = 'medium'
-plt.rcParams['figure.figsize'] = 6, 5
+plt.rcParams['figure.figsize'] = 12, 5
 plt.rcParams["legend.framealpha"] = 1
 plt.rcParams['axes.xmargin'] = 0
 plt.rcParams['axes.ymargin'] = 0
@@ -33,22 +33,24 @@ t = t.pivot('Alternative allele read count', 'Reference allele read count', 'cou
 t.sort_index(ascending=False, inplace=True)
 t.fillna(0, inplace=True)
 
+t2 = np.log10(t.copy() + 1)
+
 for l in range(min_c, max_c + 1):
     for k in range(min_c, l + 1):
         if t[k][l] + t[l][k] == 0:
             t[k][l] = 0
             t[l][k] = 0
         else:
-            t[k][l], t[l][k] = ((t[k][l] - t[l][k]) / (t[k][l] + t[l][k]) * np.log10(t[k][l] + t[l][k] + 1),
-                                (t[l][k] - t[k][l]) / (t[k][l] + t[l][k]) * np.log10(t[k][l] + t[l][k] + 1))
+            # t[k][l], t[l][k] = ((t[k][l] - t[l][k]) / (t[k][l] + t[l][k])  * np.log10(t[k][l] + t[l][k] + 1),
+            #                     (t[l][k] - t[k][l]) / (t[k][l] + t[l][k]) * np.log10(t[k][l] + t[l][k] + 1))
+            t[k][l], t[l][k] = (t[k][l] - t[l][k]) / (t[k][l] + t[l][k]), (t[l][k] - t[k][l]) / (t[k][l] + t[l][k])
 
-fig, ax = plt.subplots()
+fig, (ax1, ax2) = plt.subplots(1, 2)
 fig.tight_layout(pad=2)
-sns.heatmap(t, cmap="PRGn", vmin=-5, vmax=5)
+sns.heatmap(t, cmap="PRGn", vmin=-1, vmax=1, ax=ax1)
 
-cbar = ax.collections[0].colorbar
-cbar.set_ticks(np.arange(-5, 6, 1))
-
+cbar = ax1.collections[0].colorbar
+cbar.set_ticks(np.linspace(-1, 1, 11))
 # SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
 # SUP = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
 #
@@ -59,13 +61,13 @@ if max_c <= 50:
 else:
     div = 10
 
-ax.yaxis.set_major_locator(ticker.FixedLocator(np.arange(0, max_c - 5 + 1, div) + 0.5))
-ax.yaxis.set_major_formatter(ticker.FixedFormatter(range(0, max_c + 1)[::-div]))
-ax.tick_params(axis="y", rotation=0)
+ax1.yaxis.set_major_locator(ticker.FixedLocator(np.arange(0, max_c - 5 + 1, div) + 0.5))
+ax1.yaxis.set_major_formatter(ticker.FixedFormatter(range(0, max_c + 1)[::-div]))
+ax1.tick_params(axis="y", rotation=0)
 
-ax.xaxis.set_major_locator(ticker.FixedLocator(np.arange(0, max_c + 1, div) + 0.5))
-ax.xaxis.set_major_formatter(ticker.FixedFormatter(range(0, max_c + 1)[::div]))
-ax.tick_params(axis="x", rotation=-90)
+ax1.xaxis.set_major_locator(ticker.FixedLocator(np.arange(0, max_c + 1, div) + 0.5))
+ax1.xaxis.set_major_formatter(ticker.FixedFormatter(range(0, max_c + 1)[::div]))
+ax1.tick_params(axis="x", rotation=-90)
 
 plt.hlines(y=max_c + 1, xmin=0, xmax=max_c + 1, colors=['black', ], linewidth=lw*2 + 0.5)
 plt.vlines(x=0, ymin=0, ymax=max_c + 1, colors=['black', ], linewidth=lw*2)
@@ -75,5 +77,41 @@ plt.hlines(y=0, xmin=5, xmax=max_c + 1, colors=['#AAAAAA', ], linewidth=lw*2)
 plt.vlines(x=max_c + 1, ymin=0, ymax=max_c + 1 - 5, colors=['#AAAAAA', ], linewidth=lw*2 + 0.5)
 plt.hlines(y=max_c + 1 - 5, xmin=5-lw/10, xmax=max_c + 1, colors=['#AAAAAA', ], linewidth=lw)
 plt.vlines(x=5, ymin=0, ymax=max_c + 1 - 5 +lw/10, colors=['#AAAAAA', ], linewidth=lw)
-plt.savefig(os.path.expanduser('~/AC_2/Figure_AS_2_290120.svg'), dpi=300)
-plt.show()
+
+
+
+
+# count
+sns.heatmap(t2, cmap="Purples", vmin=0, vmax=5, ax=ax2)
+
+cbar = ax2.collections[0].colorbar
+cbar.set_ticks(np.linspace(0, 5, 6))
+# SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
+# SUP = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
+cbar.set_ticklabels(["10⁰", "10¹", "10²", "10³", "10⁴", "10⁵"])
+
+if max_c <= 50:
+    div = 5
+else:
+    div = 10
+
+ax1.yaxis.set_major_locator(ticker.FixedLocator(np.arange(0, max_c - 5 + 1, div) + 0.5))
+ax1.yaxis.set_major_formatter(ticker.FixedFormatter(range(0, max_c + 1)[::-div]))
+ax1.tick_params(axis="y", rotation=0)
+
+ax1.xaxis.set_major_locator(ticker.FixedLocator(np.arange(0, max_c + 1, div) + 0.5))
+ax1.xaxis.set_major_formatter(ticker.FixedFormatter(range(0, max_c + 1)[::div]))
+ax1.tick_params(axis="x", rotation=-90)
+
+plt.hlines(y=max_c + 1, xmin=0, xmax=max_c + 1, colors=['black', ], linewidth=lw*2 + 0.5)
+plt.vlines(x=0, ymin=0, ymax=max_c + 1, colors=['black', ], linewidth=lw*2)
+
+
+plt.hlines(y=0, xmin=5, xmax=max_c + 1, colors=['#AAAAAA', ], linewidth=lw*2)
+plt.vlines(x=max_c + 1, ymin=0, ymax=max_c + 1 - 5, colors=['#AAAAAA', ], linewidth=lw*2 + 0.5)
+plt.hlines(y=max_c + 1 - 5, xmin=5-lw/10, xmax=max_c + 1, colors=['#AAAAAA', ], linewidth=lw)
+plt.vlines(x=5, ymin=0, ymax=max_c + 1 - 5 +lw/10, colors=['#AAAAAA', ], linewidth=lw)
+
+
+
+plt.savefig(os.path.expanduser('~/AC_2/Figure_AS_2_080220.png'), dpi=300)
