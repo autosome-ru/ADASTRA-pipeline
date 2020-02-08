cc8d6ff4c7acf02bcd2a40edef8ea5f1ae6ad013
wish1 <aswq22013@gmail.com>
2020-02-08 21:30:40 +0300

    chips

--- scripts/FIGURES/AS_figure_9_chips.py
+++ scripts/FIGURES/AS_figure_9_chips.py
@@ -41,6 +41,33 @@ def get_cl(row, name):
     return row
 
 
+def get_rect(dataframe: pd.DataFrame, col_name):
+    tri_list = dataframe[col_name].to_list()
+    x = [0]
+    list = [tri_list[0]]
+    for i in range(1, len(tri_list)):
+
+        if tri_list[i] != 0 and tri_list[i-1] == 0:
+            list.append(0)
+            x.append(i - 0.5)
+            list.append(tri_list[i])
+            x.append(i - 0.5)
+
+        elif tri_list[i] == 0 and tri_list[i-1] != 0:
+            list.append(tri_list[i - 1])
+            x.append(i - 0.5)
+            list.append(0)
+            x.append(i - 0.5)
+        else:
+            list.append(tri_list[i])
+            x.append(i)
+    return x, list
+
+
+# PARAMS
+lw = 0.5
+
+
 df = pd.read_table(os.path.expanduser("~/cor_stats_test.tsv"))
 # df = df[df['total_snps'] >= 10000]
 df['color'] = df.apply(get_color, axis=1)
@@ -59,16 +86,20 @@ fig, ax = plt.subplots()
 fig.tight_layout(pad=1.5)
 # ax.margins(x=0, y=0)
 
-ax.stackplot(range(len(df.index)), df_k562["cor_by_snp_CAIC"], alpha=0.7,
-             linewidth=0.7, color='C1', labels=["K562"])
-ax.stackplot(range(len(df.index)), df_mcf7["cor_by_snp_CAIC"], alpha=0.7,
-             linewidth=0.7, color='C2', labels=["MCF7"])
-ax.stackplot(range(len(df.index)), df_other["cor_by_snp_CAIC"], alpha=0.7,
-             linewidth=0.7, color='C0', labels=["other"])
+x_k562, y_k562 = get_rect(df_k562, "cor_by_snp_CAIC")
+x_mcf7, y_mcf7 = get_rect(df_mcf7, "cor_by_snp_CAIC")
+x_other, y_other = get_rect(df_other, "cor_by_snp_CAIC")
+
+ax.stackplot(x_k562, y_k562, alpha=0.7,
+             linewidth=lw, color='C1', labels=["K562"])
+ax.stackplot(x_mcf7, y_mcf7, alpha=0.7,
+             linewidth=lw, color='C2', labels=["MCF7"])
+ax.stackplot(x_other, y_other, alpha=0.7,
+             linewidth=lw, color='C0', labels=["other"])
 
 ax.grid(True)
 ax.legend()
-plt.savefig(os.path.expanduser("~/AC_9/AS_Figure_9_cor_cosmic.png"), dpi=300)
+plt.savefig(os.path.expanduser("~/AC_9/AS_Figure_9_cor_cosmic.svg"), dpi=300)
 plt.close(fig)
 
 # Draw scatter vs COSMIC
@@ -82,7 +113,7 @@ ax.axhline(y=0, xmax=0, color='#505050', linestyle='--')
 ax.set_xlim(-1, 1)
 ax.set_ylim(-1, 1)
 ax.grid(True)
-plt.savefig(os.path.expanduser("~/AC_9/AS_Figure_9_scatter.png"), dpi=300)
+plt.savefig(os.path.expanduser("~/AC_9/AS_Figure_9_scatter.svg"), dpi=300)
 plt.close(fig)
 
 # delta tau 3 colors
@@ -95,15 +126,18 @@ df = df.dropna(subset=["cor_by_snp_probe_CGH"])
 df_k562 = df.apply(lambda x: get_cl(x, "K562"), axis=1)
 df_mcf7 = df.apply(lambda x: get_cl(x, "MCF7"), axis=1)
 df_other = df.apply(lambda x: get_cl(x, "other"), axis=1)
+x_k562, y_k562 = get_rect(df_k562, "delta_tau")
+x_mcf7, y_mcf7 = get_rect(df_mcf7, "delta_tau")
+x_other, y_other = get_rect(df_other, "delta_tau")
 
-ax.stackplot(range(len(df.index)), df_k562["delta_tau"], alpha=0.7,
-             linewidth=0.7, color='C1', labels=["K562"])
-ax.stackplot(range(len(df.index)), df_mcf7["delta_tau"], alpha=0.7,
-             linewidth=0.7, color='C2', labels=["MCF7"])
-ax.stackplot(range(len(df.index)), df_other["delta_tau"], alpha=0.7,
-             linewidth=0.7, color='C0', labels=["other"])
+ax.stackplot(x_k562, y_k562, alpha=0.7,
+             linewidth=lw, color='C1', labels=["K562"])
+ax.stackplot(x_mcf7, y_mcf7, alpha=0.7,
+             linewidth=lw, color='C2', labels=["MCF7"])
+ax.stackplot(x_other, y_other, alpha=0.7,
+             linewidth=lw, color='C0', labels=["other"])
 
 ax.grid(True)
 ax.legend()
-plt.savefig(os.path.expanduser("~/AC_9/AS_Figure_9_delta_tau_chips.png"), dpi=300)
+plt.savefig(os.path.expanduser("~/AC_9/AS_Figure_9_delta_tau_chips.svg"), dpi=300)
 plt.close(fig)
