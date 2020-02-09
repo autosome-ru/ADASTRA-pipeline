8a18e24b9d5d543310826d4b05bb470b5369490a
wish1 <aswq22013@gmail.com>
2020-02-09 12:10:40 +0300

    adjust table fixed

--- scripts/SARUSannotation/adjust_table_with_sarus.py
+++ scripts/SARUSannotation/adjust_table_with_sarus.py
@@ -70,15 +70,22 @@ with open(sys.argv[1], 'r') as table, open(sys.argv[3], 'w') as out:
 
         assert len(dict_of_snps[ID]['ref']) == len(dict_of_snps[ID]['alt'])
         assert dict_of_snps[ID]['ref'][best_idx]['pos'] == dict_of_snps[ID]['alt'][best_idx]['pos']
-        print(line[-1], line[-2])
-        out.write(pack(line + [dict_of_snps[ID]['ref'][best_idx]['p'],
-                               dict_of_snps[ID]['alt'][best_idx]['p'],
-                               dict_of_snps[ID]['alt'][best_idx]['p'] - dict_of_snps[ID]['ref'][best_idx]['p'],
-                               dict_of_snps[ID]['ref'][best_idx]['pos'],
-                               dict_of_snps[ID]['ref'][best_idx]['orientation'],
-                               get_color(float(line[-2]), float(line[-1]),
-                                         dict_of_snps[ID]['alt'][best_idx]['p'] -
-                                         dict_of_snps[ID]['ref'][best_idx]['p'],
-                                         dict_of_snps[ID]['ref'][best_idx]['p'],
-                                         dict_of_snps[ID]['alt'][best_idx]['p'])
-                               ]))
+        if line[-1] == "":
+            out.write(pack(line + [dict_of_snps[ID]['ref'][best_idx]['p'],
+                                   dict_of_snps[ID]['alt'][best_idx]['p'],
+                                   dict_of_snps[ID]['alt'][best_idx]['p'] - dict_of_snps[ID]['ref'][best_idx]['p'],
+                                   dict_of_snps[ID]['ref'][best_idx]['pos'],
+                                   dict_of_snps[ID]['ref'][best_idx]['orientation'],
+                                   "", ""]))
+        else:
+            out.write(pack(line + [dict_of_snps[ID]['ref'][best_idx]['p'],
+                                   dict_of_snps[ID]['alt'][best_idx]['p'],
+                                   dict_of_snps[ID]['alt'][best_idx]['p'] - dict_of_snps[ID]['ref'][best_idx]['p'],
+                                   dict_of_snps[ID]['ref'][best_idx]['pos'],
+                                   dict_of_snps[ID]['ref'][best_idx]['orientation'],
+                                   get_color(float(line[-2]), float(line[-1]),
+                                             dict_of_snps[ID]['alt'][best_idx]['p'] -
+                                             dict_of_snps[ID]['ref'][best_idx]['p'],
+                                             dict_of_snps[ID]['ref'][best_idx]['p'],
+                                             dict_of_snps[ID]['alt'][best_idx]['p'])
+                                   ]))
