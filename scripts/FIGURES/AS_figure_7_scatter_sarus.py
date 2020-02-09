d1fc14f706f43867629a69eb699737ac64f801ad
wish1 <aswq22013@gmail.com>
2020-02-09 13:03:02 +0300

    adjust table fixed

--- scripts/FIGURES/AS_figure_7_scatter_sarus.py
+++ scripts/FIGURES/AS_figure_7_scatter_sarus.py
@@ -37,18 +37,18 @@ if __name__ == '__main__':
     plt.rcParams['axes.ymargin'] = 0
     plt.rcParams["legend.framealpha"] = 1
 
+    field = 'fdrp_bh'
+
+    perf_tr = 0.0005
+    fc_tr = 4
+    fdr_tr = 0.005
+
+    blue_color = '#005AB5'  # 1B7837'
+    red_color = '#DC3220'  # 762A83'
+    grey_color = '#CCCCCC'
+
     for name in top10_names:
         pt = pd.read_table(os.path.expanduser("~/scatter_why_red/{}".format(name)))
-
-        field = 'fdrp_bh'
-
-        perf_tr = 0.0005
-        fc_tr = 4
-        fdr_tr = 0.005
-
-        blue_color = '#005AB5'#1B7837'
-        red_color = '#DC3220'#762A83'
-        grey_color = '#CCCCCC'
 
         pt = pt[(pt['motif_log_pref'] >= -np.log10(perf_tr)) & (pt['motif_log_palt'] >= -np.log10(perf_tr))]
         pt = pt[~(pt[field + '_alt'].isnull() | pt[field + '_ref'].isnull())]
@@ -69,11 +69,11 @@ if __name__ == '__main__':
         pt_blue = pt[pt['col'] == blue_color]
 
         fig, ax = plt.subplots()
-        plt.tight_layout(pad=1.5)
-        plt.scatter(x=pt['log_pv'], y=pt['log_fc'], c=pt['col'], s=5)
-        plt.grid(True)
-        plt.xlabel('Best -log10 fdr_p')
-        plt.ylabel('Log10 motif foldchange')
+        ax.tight_layout(pad=1.5)
+        ax.scatter(x=pt['log_pv'], y=pt['log_fc'], c=pt['col'], s=5)
+        ax.grid(True)
+        ax.xlabel('Best -log10 fdr_p')
+        ax.ylabel('Log10 motif foldchange')
         label = 'blue/red: {}/{}({:.1f}%),\ngrey/all={:.1f}%'.format(blue, red,
                                                                      100 * blue / (blue + red),
                                                                      100 * grey / (grey + blue + red))
@@ -84,3 +84,10 @@ if __name__ == '__main__':
 
         plt.savefig(os.path.expanduser("~/AC_7/AS_Figure_7_{}_p_tr={:.2f}_fc_tr={:.2f}_fdr_tr={:.2f}_{}.svg".format(
             name.replace('_fc.tsv', ''), perf_tr, fc_tr, fdr_tr, field)), dpi=300)
+        plt.close(fig)
+
+
+    fig, ax = plt.subplots()
+    plt.tight_layout(pad=1.5)
+    ax.stackplot()
+
--- scripts/QUALITYcontrol/Collect_sarus_stats.py
+++ scripts/QUALITYcontrol/Collect_sarus_stats.py
@@ -38,9 +38,9 @@ def CollectRedBlue():
                        * np.sign(pt[field + '_alt'] - pt[field + '_ref'])
 
         pt['log_fc'] = pt['motif_fc']
-        print("Df filtered")
-        pt['col'] = pt.apply(lambda x: get_color(x), axis=1)
 
+        pt['col'] = pt.apply(lambda x: get_color(x), axis=1)
+        print("Df filtered")
         red_counts = len(pt[pt['col'] == red_color].index)
         blue_counts = len(pt[pt['col'] == blue_color].index)
         print(out_df)
