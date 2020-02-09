6a267c05506c7533a536b2df224183f1fa7ae290
wish1 <aswq22013@gmail.com>
2020-02-09 12:53:59 +0300

    adjust table fixed

--- scripts/QUALITYcontrol/Collect_sarus_stats.py
+++ scripts/QUALITYcontrol/Collect_sarus_stats.py
@@ -6,8 +6,7 @@ import json
 
 sys.path.insert(1, "/home/abramov/ASB-Project")
 from scripts.HELPERS.paths_for_components import parameters_path
-from scripts.HELPERS.helpers import states, pack
-
+from scripts.HELPERS.helpers import states
 
 def get_color(row):
     if abs(row['log_fc']) < np.log10(fc_tr) or abs(row['log_pv']) < -np.log10(fdr_tr):
@@ -21,29 +20,30 @@ def get_color(row):
 
 
 def CollectRedBlue():
-    with open(os.path.expanduser("~/PARAMETERS/blue_red_stats.tsv"), "w") as out_file:
-        out_file.write(pack(["red", "blue"]))
+    out_df = pd.DataFrame(columns=["red", "blue"])
 
-        for file_name in os.listdir(agr_dir):
-            print(file_name)
-            pt = pd.read_table(agr_dir + file_name)
-            pt = pt.dropna(subset=["motif_fc"])
-            if pt.empty:
-                continue
+    for file_name in os.listdir(agr_dir):
+        print(file_name)
+        pt = pd.read_table(agr_dir + file_name)
+        if pt.empty:
+            continue
+        pt = pt.dropna(subset=["motif_fc"])
+        if pt.empty:
+            continue
+        pt = pt[(pt['motif_log_pref'] >= -np.log10(perf_tr)) & (pt['motif_log_palt'] >= -np.log10(perf_tr))]
+        pt = pt[~(pt[field + '_alt'].isnull() | pt[field + '_ref'].isnull())]
 
-            pt = pt[(pt['motif_log_pref'] >= -np.log10(perf_tr)) & (pt['motif_log_palt'] >= -np.log10(perf_tr))]
-            pt = pt[~(pt[field + '_alt'].isnull() | pt[field + '_ref'].isnull())]
+        pt['log_pv'] = (np.log10(
+            pt[[field + '_ref', field + '_alt']]).min(axis=1)) \
+                       * np.sign(pt[field + '_alt'] - pt[field + '_ref'])
 
-            pt['log_pv'] = (np.log10(
-                pt[[field + '_ref', field + '_alt']]).min(axis=1)) \
-                           * np.sign(pt[field + '_alt'] - pt[field + '_ref'])
+        pt['log_fc'] = pt['motif_fc']
+        pt['col'] = pt.apply(lambda x: get_color(x), axis=1)
 
-            pt['log_fc'] = pt['motif_fc']
-            pt['col'] = pt.apply(lambda x: get_color(x), axis=1)
-
-            red_counts = len(pt[pt['col'] == red_color].index)
-            blue_counts = len(pt[pt['col'] == blue_color].index)
-            out_file.write(pack(red_counts, blue_counts))
+        red_counts = len(pt[pt['col'] == red_color].index)
+        blue_counts = len(pt[pt['col'] == blue_color].index)
+        out_df = out_df.append(pd.DataFrame({"red": [red_counts], "blue": [blue_counts]}))
+    out_df.to_csv(os.path.expanduser("~/PARAMETERS/blue_red_stats.tsv"), sep="\t", index=False)
 
 
 if __name__ == '__main__':
