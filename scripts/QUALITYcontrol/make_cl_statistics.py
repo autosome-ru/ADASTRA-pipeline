a4af996fd90e23887e519c310f0195939385b225
wish1 <aswq22013@gmail.com>
2020-02-15 22:00:57 +0300

    statistics

--- scripts/QUALITYcontrol/make_cl_statistics.py
+++ scripts/QUALITYcontrol/make_cl_statistics.py
@@ -35,7 +35,7 @@ for line in master_list:
         continue
     num +=1
     line = line.split("\t")
-    if num%50 ==0:
+    if num % 50 == 0:
         print(num)
     # if remove_punctuation(line[4]) not in interestingSet:
     #     continue
@@ -45,14 +45,14 @@ for line in master_list:
     vcf_counter +=1
     tf_set.add(line[1])
     cl_set.add(line[4])
-    with gzip.open(vcf_path, "rt") as vcf_buffer:
-        list_of_snps = make_list_from_vcf_without_filter(vcf_buffer)
-        counter += len(list_of_snps)
-            # for chr, pos, rs_id, ref, alt, ref_counts, alt_counts in list_of_snps:
-            #     try:
-            #         SNP_statistics_dict[(ref_counts, alt_counts)] += 1
-            #     except KeyError:
-            #         SNP_statistics_dict[(ref_counts, alt_counts)] = 1
+    # with gzip.open(vcf_path, "rt") as vcf_buffer:
+    #     list_of_snps = make_list_from_vcf_without_filter(vcf_buffer)
+    #     counter += len(list_of_snps)
+    #         # for chr, pos, rs_id, ref, alt, ref_counts, alt_counts in list_of_snps:
+    #         #     try:
+    #         #         SNP_statistics_dict[(ref_counts, alt_counts)] += 1
+    #         #     except KeyError:
+    #         #         SNP_statistics_dict[(ref_counts, alt_counts)] = 1
 df = pd.DataFrame({'ref': [], 'alt': [], 'count': []})
 for ref, alt in SNP_statistics_dict:
     df = df.append(pd.DataFrame({'ref': [ref], 'alt': [alt], 'count': [SNP_statistics_dict[(ref, alt)]]}))
@@ -60,3 +60,4 @@ for ref, alt in SNP_statistics_dict:
 
 print('Total snp calls {}, different TFs {}, different cell types {}'.format(counter,
       len(tf_set), len(cl_set)))
+print(vcf_counter)
