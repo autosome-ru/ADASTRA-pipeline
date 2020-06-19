350cbdb0334a7754fee6be87c31dd85b104f5a69
wish <aswq22013@gmail.com>
2020-06-19 10:30:43 +0300

    fdr filter

--- scripts/Trash/count_rs_ids.py
+++ scripts/Trash/count_rs_ids.py
@@ -7,6 +7,7 @@ ids_set = set()
 for filename in os.listdir(dirname):
     print(filename)
     df = pd.read_table(os.path.join(dirname, filename))
+    df = df[(df['fdrp_bh_ref'] <= 0.05) | (df['fdrp_bh_alt'] <= 0.05)]
     ids_set |= set(df['ID'])
 
 print(len(ids_set))
