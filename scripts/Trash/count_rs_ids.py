69ee867507e769d069361a13009b1fdb5fd6d58e
wish <aswq22013@gmail.com>
2020-06-19 10:20:06 +0300

    count rs ids script

---
+++ scripts/Trash/count_rs_ids.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import pandas as pd
+
+dirname = sys.argv[1]
+ids_set = set()
+for filename in os.listdir(dirname):
+    df = pd.read_table(os.path.join(dirname, filename))
+    ids_set += set(df['ID'])
+
+print(len(ids_set))
