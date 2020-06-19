a3357b979e679f677383f6644053587295093041
wish <aswq22013@gmail.com>
2020-06-19 10:23:45 +0300

    |=

--- scripts/Trash/count_rs_ids.py
+++ scripts/Trash/count_rs_ids.py
@@ -5,6 +5,7 @@ import pandas as pd
 dirname = sys.argv[1]
 ids_set = set()
 for filename in os.listdir(dirname):
+    print(filename)
     df = pd.read_table(os.path.join(dirname, filename))
     ids_set |= set(df['ID'])
 
