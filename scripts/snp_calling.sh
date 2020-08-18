3349e996210a8821b4c0d08e90d19d06061420f2
wish1 <aswq22013@gmail.com>
2020-08-18 22:19:33 +0300

    debug

--- scripts/snp_calling.sh
+++ scripts/snp_calling.sh
@@ -3,5 +3,5 @@
 source scripts/HELPERS/paths_for_components.py
 njobs=$1
 
-parallel --delay 80 --jobs "$njobs" \
+parallel --delay 20 --jobs "$njobs" \
 bash "$scripts_path/SNPcalling/ProcessLine.sh" :::: "${parallel_parameters_path}/sorted_master_list.tsv"
