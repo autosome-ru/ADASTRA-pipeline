185020b9100f20a3f2e9daa4746b071dda1f5fc5
wish1 <aswq22013@gmail.com>
2020-08-18 11:59:36 +0300

    setup

--- scripts/setup.py
+++ scripts/setup.py
@@ -5,24 +5,24 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'badmaps_dict = PARAMETERS.make_badmaps_dict:main',
-            'sort_cols = SNPcalling.sort_columns:main',
-            'init_dirs = PARAMETERS.create_initial_dirs:main',
-            'aggregation_dict = PARAMETERS.make_aggregation_dict:main',
-            'make_paths = PARAMETERS.make_exp_paths_from_master_list:main',
-            'badmaps_params = PARAMETERS.make_params_bad_estimation:main',
-            'aggregation_params = PARAMETERS.make_params_aggregation:main',
-            'sort_params = PARAMETERS.sort_params:main',
-            'check_pos_peaks = PEAKannotation.check_pos_peaks:main',
-            'annotate_peaks = PEAKannotation.annotate:main'
+            'ADASTRA badmaps_dict = PARAMETERS.make_badmaps_dict:main',
+            'ADASTRA sort_cols = SNPcalling.sort_columns:main',
+            'ADASTRA init_dirs = PARAMETERS.create_initial_dirs:main',
+            'ADASTRA aggregation_dict = PARAMETERS.make_aggregation_dict:main',
+            'ADASTRA make_paths = PARAMETERS.make_exp_paths_from_master_list:main',
+            'ADASTRA badmaps_params = PARAMETERS.make_params_bad_estimation:main',
+            'ADASTRA aggregation_params = PARAMETERS.make_params_aggregation:main',
+            'ADASTRA sort_params = PARAMETERS.sort_params:main',
+            'ADASTRA check_pos_peaks = PEAKannotation.check_pos_peaks:main',
+            'ADASTRA annotate_peaks = PEAKannotation.annotate:main'
 
-            'vcf_merge = BADcalling.VCFMerger:main',
-            'bad_call = BADcalling.BADestimation:main',
-            'bad_annotation = ASBcalling.BAD_annotation:main',
-            'collect_ref_bias = FITnoise.collect_ref_bias_statistics:main',
-            'fit_neg_bin = FITnoise.fit_negative_binom_with_weights:main',
-            'neg_bin_p = ASBcalling.NBpcounter:main',
-            'aggregation = ASBcalling.Aggregation:main'
+            'ADASTRA vcf_merge = BADcalling.VCFMerger:main',
+            'ADASTRA bad_call = BADcalling.BADestimation:main',
+            'ADASTRA bad_annotation = ASBcalling.BAD_annotation:main',
+            'ADASTRA collect_ref_bias = FITnoise.collect_ref_bias_statistics:main',
+            'ADASTRA fit_neg_bin = FITnoise.fit_negative_binom_with_weights:main',
+            'ADASTRA neg_bin_p = ASBcalling.NBpcounter:main',
+            'ADASTRA aggregation = ASBcalling.Aggregation:main'
         ],
     },
     author="Sergey Abramov, Alexandr Boytsov",
