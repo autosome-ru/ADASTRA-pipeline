99f273ed61642d315754ddcb2fc3e677963ea317
m1kss <boytsovs.av@phystech.edu>
2020-09-24 21:53:09 +0300

    count model

--- scripts/QUALITYcontrol/make_counts_for_model.py
+++ scripts/QUALITYcontrol/make_counts_for_model.py
@@ -4,10 +4,10 @@ import sys
 import numpy as np
 
 sys.path.insert(1, "/home/abramov/segmentationValidation/ADASTRA-pipeline")
-from scripts.HELPERS.helpers import states
-from scripts.HELPERS.paths_for_components import parameters_path
+from scripts.HELPERS.helpers import get_states
 
 for model in os.listdir(os.path.expanduser('~/HeatmapData_val/')):
+    states = get_states(model.split('@')[1])
     df = pd.read_table(os.path.expanduser(os.path.expanduser('~/{}.tsv'.format(model))))
     print('iread {}'.format(model))
     df.columns = ['chr', 'pos', 'cov', 'BAD', 'COSMIC'] + ['Q{:.2f}'.format(state) for state in states]
