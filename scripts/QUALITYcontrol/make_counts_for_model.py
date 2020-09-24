9f79e8220c2402da543a540f59ce5367fd807518
m1kss <boytsovs.av@phystech.edu>
2020-09-24 21:54:53 +0300

    count model

--- scripts/QUALITYcontrol/make_counts_for_model.py
+++ scripts/QUALITYcontrol/make_counts_for_model.py
@@ -8,10 +8,10 @@ from scripts.HELPERS.helpers import get_states
 
 for model in os.listdir(os.path.expanduser('~/HeatmapData_val/')):
     states = get_states(model.split('@')[1])
-    df = pd.read_table(os.path.expanduser(os.path.expanduser('~/{}.tsv'.format(model))))
+    df = pd.read_table(os.path.expanduser('~/{}.tsv'.format(model)))
     print('iread {}'.format(model))
     df.columns = ['chr', 'pos', 'cov', 'BAD', 'COSMIC'] + ['Q{:.2f}'.format(state) for state in states]
     df = df[['BAD', 'COSMIC']]
     df_counts = df.groupby(['BAD', 'COSMIC']).size().reset_index(name='counts')
     df_counts = df_counts.groupby(['BAD', 'COSMIC'], as_index=False)['counts'].sum()
-    df_counts.to_csv(os.path.expanduser('~/PARAMETERS/counts/counts_{}.tsv'.format(model)), index=False, sep='\t')
+    df_counts.to_csv(os.path.expanduser('~/PARAMETERS/counts_val/counts_{}.tsv'.format(model)), index=False, sep='\t')
