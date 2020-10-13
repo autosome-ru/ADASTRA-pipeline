4e926340852e2f3a5ff63e67b926ed249fa2e9f3
m1kss <boytsovs.av@phystech.edu>
2020-10-13 07:48:05 +0300

    fix path join

--- scripts/PARAMETERS/make_params_correlation.py
+++ scripts/PARAMETERS/make_params_correlation.py
@@ -7,8 +7,8 @@ from scripts.HELPERS.paths_for_components import parallel_parameters_path, corre
 def main():
     with open(os.path.join(parallel_parameters_path, 'CS_parameters.cfg'), 'w') as file:
         for file_name in os.listdir(correlation_path):
-            if os.path.isdir(correlation_path + file_name):
-                for file_name2 in os.listdir(correlation_path + file_name):
+            if os.path.isdir(os.path.join(correlation_path, file_name)):
+                for file_name2 in os.listdir(os.path.join(correlation_path, file_name)):
                     file.write(file_name2 + '\n')
                 break
 
