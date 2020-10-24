359e9896e8af8eaa0c46ab4635c6515cfd921494
wish1 <aswq22013@gmail.com>
2020-10-24 19:34:42 +0300

    sarus fixes

--- scripts/SARUSannotation/extract_sarus_data.py
+++ scripts/SARUSannotation/extract_sarus_data.py
@@ -11,7 +11,7 @@ nuc = dict(zip([1, 2, 3, 4, 5], ['a', 'c', 'g', 't', 'N']))
 
 
 def main(tf_name, motif_length):
-    if motif_length is not None:
+    if motif_length is None:
         exit(0)
     tf_path = os.path.join(results_path, 'TF_P-values', tf_name + '.tsv')
     out_path = get_tf_sarus_path(tf_name, 'fasta')
