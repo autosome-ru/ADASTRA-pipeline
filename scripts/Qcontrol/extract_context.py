e46d004a3b1ff25c1b6928fd3c0d7a119e39f2a4
wish1 <aswq22013@gmail.com>
2020-11-02 12:41:16 +0300

    extract sarus data

--- scripts/Qcontrol/extract_context.py
+++ scripts/Qcontrol/extract_context.py
@@ -10,6 +10,8 @@ def main():
         tf_name = os.path.splitext(tf_file)[0]
         tf_df = pd.read_table(get_result_table_path('TF', tf_name))
         tf_df = tf_df[(tf_df['fdrp_bh_ref'] <= 0.05) | (tf_df['fdrp_bh_alt'] <= 0.05)]
+        if tf_df.empty:
+            continue
         tf_df['unique'] = '{}@{}{}'.format(all_asbs_df['#chr'],
                                            all_asbs_df['pos'],
                                            all_asbs_df['alt'])
