73a083e6cacdf4e299021d8c3dc589ce103884a6
wish1 <aswq22013@gmail.com>
2020-11-02 16:17:13 +0300

    extract sarus data

--- scripts/Qcontrol/extract_context.py
+++ scripts/Qcontrol/extract_context.py
@@ -6,17 +6,19 @@ from scripts.SARUSannotation.extract_sarus_data import main as main_extract
 
 def main():
     all_asbs_df = None
-    for tf_file in os.listdir(get_result_dir_path('TF')):
-        tf_name = os.path.splitext(tf_file)[0]
-        tf_df = pd.read_table(get_result_table_path('TF', tf_name))
-        tf_df = tf_df[(tf_df['fdrp_bh_ref'] <= 0.05) | (tf_df['fdrp_bh_alt'] <= 0.05)]
-        if tf_df.empty:
-            continue
-        tf_df['unique'] = '{}@{}{}'.format(tf_df['#chr'],
-                                           tf_df['pos'],
-                                           tf_df['alt'])
-        if all_asbs_df is None:
-            all_asbs_df = tf_df
-        else:
-            all_asbs_df = all_asbs_df.append(tf_df, ignore_index=True)
+    for obj in 'TF', 'CL':
+        for tf_file in os.listdir(get_result_dir_path(obj)):
+            tf_name = os.path.splitext(tf_file)[0]
+            tf_df = pd.read_table(get_result_table_path(obj, tf_name))
+            tf_df = tf_df.columns.drop(list(tf_df.filter(regex='motif')))
+            tf_df = tf_df[(tf_df['fdrp_bh_ref'] <= 0.05) | (tf_df['fdrp_bh_alt'] <= 0.05)]
+            if tf_df.empty:
+                continue
+            tf_df['unique'] = '{}@{}{}'.format(tf_df['#chr'],
+                                               tf_df['pos'],
+                                               tf_df['alt'])
+            if all_asbs_df is None:
+                all_asbs_df = tf_df
+            else:
+                all_asbs_df = all_asbs_df.append(tf_df, ignore_index=True)
     main_extract('all_tfs', 25, all_asbs_df.sort_values(by=['pos', '#chr']))
