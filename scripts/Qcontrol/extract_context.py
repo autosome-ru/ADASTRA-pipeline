50c5ab44d6f161c0f1988611fa7064bee4df9b84
wish1 <aswq22013@gmail.com>
2020-11-02 12:39:09 +0300

    extract sarus data

--- scripts/Qcontrol/extract_context.py
+++ scripts/Qcontrol/extract_context.py
@@ -10,9 +10,13 @@ def main():
         tf_name = os.path.splitext(tf_file)[0]
         tf_df = pd.read_table(get_result_table_path('TF', tf_name))
         tf_df = tf_df[(tf_df['fdrp_bh_ref'] <= 0.05) | (tf_df['fdrp_bh_alt'] <= 0.05)]
+        tf_df['unique'] = '{}@{}{}'.format(all_asbs_df['#chr'],
+                                           all_asbs_df['pos'],
+                                           all_asbs_df['alt'])
         if all_asbs_df is None:
             all_asbs_df = tf_df
         else:
-            all_asbs_df.append(tf_df, ignore_index=True)
+            all_asbs_df = all_asbs_df.append(tf_df, ignore_index=True)
+        all_asbs_df.drop_duplicates(subset='unique')
         print(len(all_asbs_df.index))
     main_extract('all_tfs', 25, all_asbs_df)
