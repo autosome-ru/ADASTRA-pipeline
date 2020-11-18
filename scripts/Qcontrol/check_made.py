6339ecab1efafc0a07da6b3e0b5cdb9f72daf365
wish1 <aswq22013@gmail.com>
2020-11-19 01:24:27 +0300

    move dnase to nas

--- scripts/Qcontrol/check_made.py
+++ scripts/Qcontrol/check_made.py
@@ -13,7 +13,6 @@ def main():
     made_control_vcfs = 0
 
     made_annotated_tables = 0
-    dict_concordance_stats = {}
     dict_overall_statistics = {"SNP_calls": None, "unique_SNPs": None,
                                "unique_asb": None, "datasets": None,
                                "unique_SNPs_rs": None, "unique_asb_rs": None,
@@ -30,12 +29,9 @@ def main():
     ml_df = ml_df[ml_df['vcf_path'].apply(os.path.isfile)]
     for index, row in ml_df.iterrows():
         row['CELLS'] = remove_punctuation(row['CELLS'])
-        if row['TF_UNIPROT_NAME'] not in dict_overall_statistics["datasets"]["TF"]:
-            dict_overall_statistics["datasets"]["TF"][row['TF_UNIPROT_NAME']] = 0
         if row['CELLS'] not in dict_overall_statistics["datasets"]["CL"]:
             dict_overall_statistics["datasets"]["CL"][row['CELLS']] = 0
         dict_overall_statistics["datasets"]["CL"][row['CELLS']] += 1
-        dict_overall_statistics["datasets"]["TF"][row['TF_UNIPROT_NAME']] += 1
         if row['EXP_TYPE'] == 'chip_control':
             made_control_vcfs += 1
         else:
@@ -51,12 +47,9 @@ def main():
         local_counter = len(an_table.index)
         if local_counter == 0:
             continue
-        if row['TF_UNIPROT_NAME'] not in dict_overall_statistics["SNP_calls"]["TF"]:
-            dict_overall_statistics["SNP_calls"]["TF"][row['TF_UNIPROT_NAME']] = 0
         if row['CELLS'] not in dict_overall_statistics["SNP_calls"]["CL"]:
             dict_overall_statistics["SNP_calls"]["CL"][row['CELLS']] = 0
         dict_overall_statistics["SNP_calls"]["CL"][row['CELLS']] += local_counter
-        dict_overall_statistics["SNP_calls"]["TF"][row['TF_UNIPROT_NAME']] += local_counter
 
     print("Made {}/{} VCFs ({} experiment VCFs, {} control VCFs), {} annotated tables".format(
         made_control_vcfs + made_experiment_vcfs, total_vcf_count,
@@ -67,7 +60,7 @@ def main():
     obj_counter = {'TF': 0, 'CL': 0}
     total_fdrs_ref = 0
     total_fdrs_alt = 0
-    for what_for in ('TF', 'CL'):
+    for what_for in ('CL', ):
         for obj in os.listdir(get_result_dir_path(what_for)):
             obj_counter[what_for] += 1
             obj_table = pd.read_table(get_result_table_path(what_for, os.path.splitext(obj)[0]))
@@ -94,25 +87,6 @@ def main():
         print("Made aggregation for {} {}s".format(obj_counter[what_for], what_for))
         print('In {} aggregation - {},{} ref and alt ASB events respectively'.format(what_for,
                                                                                      total_fdrs_ref, total_fdrs_alt))
-    for tf_file in os.listdir(get_result_dir_path('TF')):
-        tf_name = os.path.splitext(tf_file)[0]
-        dict_concordance_stats[tf_name] = {
-            "Concordant": 0,
-            "Discordant": 0,
-            "Weak Concordant": 0,
-            "Weak Discordant": 0,
-            "No Hit": 0
-        }
-        tf_df = pd.read_table(get_result_table_path('TF', tf_name))
-        tf_df = tf_df[(tf_df['fdrp_bh_ref'] <= 0.05) | (tf_df['fdrp_bh_alt'] <= 0.05)]
-        if tf_df.empty:
-            continue
-        d = tf_df['motif_conc'].value_counts().to_dict()
-        for concordance in dict_concordance_stats[tf_name]:
-            if d.get(concordance) is not None:
-                dict_concordance_stats[tf_name][concordance] = d[concordance]
-    with open(os.path.join(get_release_stats_path(), "concordance_statistics.json"), "w") as conc_file:
-        json.dump(dict_concordance_stats, conc_file)
     with open(os.path.join(get_release_stats_path(), "overall_statistics.json"), "w") as json_file:
         json.dump(dict_overall_statistics, json_file)
 
