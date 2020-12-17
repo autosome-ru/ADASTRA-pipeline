18ad2bbeb34e87b78dd893ee51d3349f3726658d
wish1 <aswq22013@gmail.com>
2020-12-18 00:14:14 +0300

    move dnase to nas

--- scripts/PARAMETERS/make_exp_paths_from_master_list.py
+++ scripts/PARAMETERS/make_exp_paths_from_master_list.py
@@ -4,14 +4,13 @@ import sys
 
 from scripts.HELPERS.helpers import dtype_dict
 from scripts.HELPERS.paths import get_ending, create_path_from_master_list_df
-from scripts.HELPERS.paths_for_components import parallel_parameters_path, badmaps_dict_path, master_list_path
+from scripts.HELPERS.paths_for_components import parallel_parameters_path, master_list_path
 
 out_path = os.path.join(parallel_parameters_path, 'exp_paths.cfg')
 
 
 def main(for_what):
     master_df = pd.read_table(master_list_path, dtype=dtype_dict)
-    master_df = master_df[~master_df['EXP_TYPE'].isin(['chip_control', 'chipexo_control'])]
     master_df['path'] = master_df.apply(create_path_from_master_list_df, axis=1)
     master_df = master_df[master_df['path'].apply(
         lambda x: os.path.isfile(x + get_ending('vcf')))]
