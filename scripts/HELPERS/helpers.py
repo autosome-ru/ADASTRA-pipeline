0d8e6f4ea3bee8f08700f92d53afac207014d845
m1kss <boytsovs.av@phystech.edu>
2020-12-24 00:56:04 +0300

    debug

--- scripts/HELPERS/helpers.py
+++ scripts/HELPERS/helpers.py
@@ -343,7 +343,7 @@ class UnpackBadSegments:
 
         if UnpackBadSegments.counter is not None:
             UnpackBadSegments.counter += 1
-            return [line[0], int(line[1]), int(line[2]), float(line[3]), UnpackBadSegments.counter] + \
+            return [line[0], int(line[1]), int(line[2]), float(line[3])] + \
                    [dict(zip(states, line[4: 4 + len(states)]))] + line[(4 + len(states)):]
         else:
             return [line[0], int(line[1]), int(line[2]), float(line[3])] + \
