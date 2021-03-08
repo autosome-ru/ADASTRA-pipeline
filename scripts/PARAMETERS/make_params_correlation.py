679e92de536b77066716458b11682a5edd071524
Sashok <boytsovs@mail.ru>
2021-03-09 00:26:41 +0300

    correlation path change to BADmaps

--- scripts/PARAMETERS/make_params_correlation.py
+++ scripts/PARAMETERS/make_params_correlation.py
@@ -1,14 +1,15 @@
 import os
 import sys
 
-from scripts.HELPERS.paths_for_components import parallel_parameters_path, correlation_path
+from scripts.HELPERS.paths import get_correlation_path
+from scripts.HELPERS.paths_for_components import parallel_parameters_path
 
 
 def main():
     with open(os.path.join(parallel_parameters_path, 'CS_parameters.cfg'), 'w') as file:
-        for file_name in os.listdir(correlation_path):
-            if os.path.isdir(os.path.join(correlation_path, file_name)) and file_name.endswith('_tables'):
-                for file_name2 in os.listdir(os.path.join(correlation_path, file_name)):
+        for file_name in os.listdir(get_correlation_path()):
+            if os.path.isdir(os.path.join(get_correlation_path(), file_name)) and file_name.endswith('_tables'):
+                for file_name2 in os.listdir(os.path.join(get_correlation_path(), file_name)):
                     file.write(file_name2 + '\n')
                 break
 
