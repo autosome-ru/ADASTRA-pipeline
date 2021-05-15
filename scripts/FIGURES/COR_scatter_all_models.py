18ff2f17ef0d47036e047940e5c2a7423e08825f
Sashok <boytsovs@mail.ru>
2021-05-15 18:52:29 +0300

    new features for BAD metrics collection

---
+++ scripts/FIGURES/COR_scatter_all_models.py
@@ -0,0 +1,252 @@
+import sys
+import os
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt, ticker
+import seaborn as sns
+
+
+def get_hue(row):
+    if row['#cell_line'] == 'K562__myelogenous_leukemia_':
+        return 'K562__myelogenous_leukemia_'
+    elif row['#cell_line'] == "MCF7__Invasive_ductal_breast_carcinoma_":
+        return "MCF7__Invasive_ductal_breast_carcinoma_"
+    elif row['#cell_line'] == 'A549__lung_carcinoma_':
+        return 'A549__lung_carcinoma_'
+    elif row['#cell_line'] == '22RV1__prostate_carcinoma_':
+        return '22RV1__prostate_carcinoma_'
+    else:
+        return "others"
+
+
+def get_color(row):
+    if row['#cell_line'] == 'K562__myelogenous_leukemia_':
+        return 'C1'
+    elif row['#cell_line'] == "MCF7__Invasive_ductal_breast_carcinoma_":
+        return "C2"
+    elif row['#cell_line'] == 'A549__lung_carcinoma_':
+        return 'C3'
+    elif row['#cell_line'] == '22RV1__prostate_carcinoma_':
+        return 'C4'
+    else:
+        return "C0"
+
+
+def get_cl(row, name, model):
+    if name == "K562":
+        if row['#cell_line'] != 'K562__myelogenous_leukemia_':
+            row[model] = 0
+            row["delta_tau"] = 0
+    elif name == "MCF7":
+        if row['#cell_line'] != "MCF7__Invasive_ductal_breast_carcinoma_":
+            row[model] = 0
+            row["delta_tau"] = 0
+    elif name == 'A549':
+        if row['#cell_line'] != 'A549__lung_carcinoma_':
+            row[model] = 0
+            row["delta_tau"] = 0
+    elif name == '22RV1':
+        if row['#cell_line'] != '22RV1__prostate_carcinoma_':
+            row[model] = 0
+            row["delta_tau"] = 0
+    elif name == "other":
+        if row['#cell_line'] == "MCF7__Invasive_ductal_breast_carcinoma_" or \
+                row['#cell_line'] == 'K562__myelogenous_leukemia_' or row['#cell_line'] == 'A549__lung_carcinoma_':
+            row[model] = 0
+            row["delta_tau"] = 0
+    return row
+
+
+def get_rect(dataframe: pd.DataFrame, col_name):
+    tri_list = dataframe[col_name].to_list()
+    x = [0]
+    list = [tri_list[0]]
+    for i in range(1, len(tri_list)):
+
+        if tri_list[i] != 0 and tri_list[i-1] == 0:
+            list.append(0)
+            x.append(i - 0.5)
+            list.append(tri_list[i])
+            x.append(i - 0.5)
+
+        elif tri_list[i] == 0 and tri_list[i-1] != 0:
+            list.append(tri_list[i - 1])
+            x.append(i - 0.5)
+            list.append(0)
+            x.append(i - 0.5)
+        else:
+            list.append(tri_list[i])
+            x.append(i)
+    return x, list
+
+
+sns.set(font_scale=1.4, style="ticks", font="lato",
+        # palette=('#56B4E9', '#009E73', '#F0E442'))
+        # palette=('#7570b3', '#d95f02', '#1b9e77'))
+        palette=('#56B4E9', '#E69F00', '#009E73', '#D55E00', '#CC79A7'))
+        # palette=('#1f77b4', '#2ca02c', '#ff7f0e'))
+sns.set_style({"xtick.direction": "in", "ytick.direction": "in"})
+plt.rcParams['font.weight'] = "medium"
+plt.rcParams['axes.labelweight'] = 'medium'
+plt.rcParams['figure.titleweight'] = 'medium'
+plt.rcParams['axes.titleweight'] = 'medium'
+plt.rcParams['figure.figsize'] = 6, 5
+plt.rcParams["legend.framealpha"] = 0.5
+plt.rcParams['axes.xmargin'] = 0
+plt.rcParams['axes.ymargin'] = 0
+plt.rcParams["legend.framealpha"] = 1
+
+
+# PARAMS
+lw = 0.05
+a = 0.3
+a1 = 0.8
+states = []
+mults = []
+k562 = []
+mcf7 = []
+a549 = []
+rv1 = []
+other = []
+for sig in ('int_6', 'full_5_and_6', 'full_6_but_1.33', 'full_6_but_2.5', 'full_6'):
+    for mult in [3, 4, 5]:
+        print(sig, mult)
+        model = "cor_by_snp_CAIC@{}@{}".format(sig, mult)
+
+        df = pd.read_table(os.path.expanduser("D:\Sashok/Desktop/susan_BAD/cor_stats_test.tsv"))
+        # df = df[df['total_snps'] >= 10000]
+        # for x in df['#cell_line'].unique():
+        #     print(x)
+        df['color'] = df.apply(get_color, axis=1)
+        df['hue'] = df.apply(get_hue, axis=1)
+        df = df.sort_values('color', axis=0)
+        # df = df.fillna(value=0.01)
+        df = df.dropna(subset=[model])
+        df = df.sort_values(model, axis=0, ascending=False)
+        df["delta_tau"] = df[model] - df["cor_by_snp_probe_CGH"]
+
+        df = df[df[model] != 0]
+
+        df_k562 = df.apply(lambda x: get_cl(x, "K562", model), axis=1)
+        df_mcf7 = df.apply(lambda x: get_cl(x, "MCF7", model), axis=1)
+        df_a549 = df.apply(lambda x: get_cl(x, "A549", model), axis=1)
+        df_22rv1 = df.apply(lambda x: get_cl(x, "22RV1", model), axis=1)
+        df_other = df.apply(lambda x: get_cl(x, "other", model), axis=1)
+
+        # # Draw 3 colors for cell lines vs cosmic
+        # fig, ax = plt.subplots()
+        # fig.tight_layout(pad=2)
+        # # ax.margins(x=0, y=0)
+        #
+        # x_k562, y_k562 = get_rect(df_k562, model)
+        # x_mcf7, y_mcf7 = get_rect(df_mcf7, model)
+        # x_other, y_other = get_rect(df_other, model)
+        # x_a549, y_a549 = get_rect(df_a549, model)
+        #
+        # ax.stackplot(x_k562, y_k562, alpha=a1,
+        #              linewidth=lw, color='C1', labels=["K562"])
+        # ax.stackplot(x_mcf7, y_mcf7, alpha=a1,
+        #              linewidth=lw, color='C2', labels=["MCF7"])
+        # ax.stackplot(x_other, y_other, alpha=a1,
+        #              linewidth=lw, color='C0', labels=["Other"])
+        # ax.stackplot(x_a549, y_a549, alpha=a1,
+        #              linewidth=lw, color='C3', labels=["A549"])
+        #
+        # ax.grid(True)
+        # ax.legend()
+        # ax.set_ylabel("Kendall's τ (Segmentation, COSMIC)")
+        # ax.set_xlabel("Dataset groups sorted by τ")
+        # ax.set_ylim(-0.4, 1)
+        #
+        # plt.title('{}@{:.1f}'.format(sig, mult))
+        #
+        # plt.savefig(os.path.expanduser('D:\Sashok/Desktop/susan_BAD/stack_plot@{}@{:.1f}.png'.format(sig, mult)), dpi=300)
+        # plt.close(fig)
+        #
+        # # Draw 3 colors for cell lines vs cosmic separately
+        # fig, ax = plt.subplots()
+        # fig.tight_layout(pad=2)
+        # # ax.margins(x=0, y=0)
+        #
+        # ax.hist(df_k562[(df_k562[model] != 0) & (df_k562['total_snps'] > 1000)][model], alpha=a, linewidth=lw,  color='C1', label="K562", bins=50, range=(-0.4, 1))
+        # ax.hist(df_mcf7[(df_mcf7[model] != 0) & (df_mcf7['total_snps'] > 1000)][model], alpha=a, linewidth=lw, color='C2', label="MCF7", bins=50, range=(-0.4, 1))
+        # ax.hist(df_other[(df_other[model] != 0) & (df_other['total_snps'] > 1000)][model], alpha=a, linewidth=lw, color='C0', label="Other", bins=50, range=(-0.4, 1))
+        # ax.hist(df_a549[(df_a549[model] != 0) & (df_a549['total_snps'] > 1000)][model], alpha=a, linewidth=lw, color='C3', label="A549", bins=50, range=(-0.4, 1))
+
+        print(model)
+        print('K562: {:.3f}'.format(np.mean(df_k562[(df_k562[model] != 0) & (df_k562[model] != 0.01)][model])))
+        print('MCF7: {:.3f}'.format(np.mean(df_mcf7[(df_mcf7[model] != 0) & (df_mcf7[model] != 0.01)][model])))
+        print('A549: {:.3f}'.format(np.mean(df_a549[(df_a549[model] != 0) & (df_a549[model] != 0.01)][model])))
+        print('22RV1: {:.3f}'.format(np.mean(df_22rv1[(df_a549[model] != 0) & (df_22rv1[model] != 0.01)][model])))
+        print('Other: {:.3f}'.format(np.mean(df_other[(df_other[model] != 0) & (df_other[model] != 0.01)][model])))
+
+        states.append(sig)
+        mults.append(mult)
+        k562.append(np.quantile(df_k562[(df_k562[model] != 0) & (df_k562[model] != 0.01)][model], 0.75))
+        mcf7.append(np.quantile(df_mcf7[(df_mcf7[model] != 0) & (df_mcf7[model] != 0.01)][model], 0.75))
+        a549.append(np.quantile(df_a549[(df_a549[model] != 0) & (df_a549[model] != 0.01)][model], 0.75))
+        rv1.append(np.quantile(df_a549[(df_22rv1[model] != 0) & (df_22rv1[model] != 0.01)][model], 0.75))
+        other.append(np.quantile(df_other[(df_other[model] != 0) & (df_other[model] != 0.01)][model], 0.75))
+        #
+        # ax.grid(True)
+        # ax.legend()
+        # ax.set_ylabel("Dataset groups count")
+        # ax.set_xlabel("Kendall's τ (Segmentation, COSMIC)")
+        #
+        # plt.title('{}@{:.1f}'.format(sig, mult))
+        #
+        # plt.savefig(os.path.expanduser('D:\Sashok/Desktop/susan_BAD/hist_plot@{}@{:.1f}.png'.format(sig, mult)), dpi=300)
+        # plt.close(fig)
+
+        # Draw scatter vs COSMIC
+        fig, ax = plt.subplots()
+        fig.tight_layout(pad=2)
+
+        df_k562 = df[df['color'] == 'C1']
+        df_mcf7 = df[df['color'] == 'C2']
+        df_a549 = df[df['color'] == 'C3']
+        df_22rv1 = df[df['color'] == 'C4']
+        df_other = df[df['color'] == 'C0']
+
+        field = 'total_snps'
+
+        sc1 = plt.scatter(y=df_k562[model], x=df_k562[field], zorder=1,
+                        linewidth=0, alpha=0.7, color='C1', label='K562')
+        sc2 = plt.scatter(y=df_mcf7[model], x=df_mcf7[field], zorder=1,
+                        linewidth=0, alpha=0.7, color='C2', label='MCF7')
+        sc3 = plt.scatter(y=df_other[model], x=df_other[field], zorder=1,
+                        linewidth=0, alpha=0.7, color='C0', label='Other')
+        sc4 = plt.scatter(y=df_a549[model], x=df_a549[field], zorder=1,
+                        linewidth=0, alpha=0.7, color='C3', label='A549')
+        sc5 = plt.scatter(y=df_22rv1[model], x=df_22rv1[field], zorder=1,
+                        linewidth=0, alpha=0.7, color='C4', label='22RV1')
+        sns.lineplot(x=[-1, 1], y=[-1, 1], color='#505050')
+        ax.axvline(x=0, color='#505050', linestyle='--')
+        ax.axhline(y=0, color='#505050', linestyle='--')
+        ax.legend(loc='lower right', handletextpad=0.3, handlelength=1)
+        # ax.set_xticks([-0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1])
+        ax.set_yticks([-1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1])
+        ax.set_xscale('log')
+        ax.set_xlim(100, 20000000)
+        ax.set_ylim(-0.5, 1)
+        ax.grid(True)
+
+        ax.set_ylabel("Kendall's τ (Segmentation, COSMIC)")
+        ax.set_xlabel("Number of SNPs in a group of datasets")
+
+        plt.title('{}@{:.1f}'.format(sig, mult))
+
+        plt.savefig(os.path.expanduser('D:\Sashok/Desktop/susan_BAD/cor_plot@{}@{:.1f}.png'.format(sig, mult)), dpi=300)
+        plt.close(fig)
+
+df = pd.DataFrame({
+    'States': states,
+    'Multiplier': mults,
+    'K562_Q3': k562,
+    'MCF7_Q3': mcf7,
+    'A549_Q3': a549,
+    '22RV1_Q3': rv1,
+    'Other': other,
+})
+
+df.to_csv(os.path.expanduser('D:\Sashok/Desktop/susan_BAD/stats.tsv'), sep='\t', index=False)
---
+++ scripts/Qcontrol/collect_cell_line_wise_data_for_ROC.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import os
+import numpy as np
+
+from scripts.HELPERS.helpers import get_states
+from scripts.HELPERS.paths import get_heatmap_data_path
+
+cell_sign_dict = {
+    'K562': 'K562__myelogenous_leukemia_',
+    'MCF7': "MCF7__Invasive_ductal_breast_carcinoma_",
+    'A549': 'A549__lung_carcinoma_',
+    'HCT116': 'HCT-116__colon_carcinoma_',
+    '22RV1': '22RV1__prostate_carcinoma_',
+}
+
+
+def cell_line_in_file_from_sign(cell_sign, file):
+    for cell_line, sign in cell_sign_dict.items():
+        if cell_line in file and cell_sign == sign:
+            return True
+    if cell_sign == 'Other':
+        return True
+    else:
+        return False
+
+
+def main(states_sign, b_penalty):
+    model = 'CAIC@{}@{}'.format(states_sign, b_penalty)
+    print(model)
+    for cell_sign in ('K562', 'MCF7', 'A549', 'HCT116', '22RV1', 'Other'):
+        print(cell_sign)
+        states = get_states(states_sign)
+        heatmap_dir = os.path.join(get_heatmap_data_path(), model + '_tables/')
+        dfs = []
+        for file in os.listdir(heatmap_dir):
+            if not cell_line_in_file_from_sign(cell_sign, file):
+                continue
+            try:
+                dfs.append(pd.read_table(os.path.join(heatmap_dir, file), header=None, comment='#'))
+            except pd.errors.EmptyDataError:
+                continue
+        full_df = pd.concat(dfs)
+        full_df.columns = ['chr', 'pos', 'cov', 'BAD', 'COSMIC'] + ['Q{:.2f}'.format(state) for state in states]
+        print('i read')
+
+        for BAD in states:
+            df = full_df.copy()
+            df['BAD'] = BAD
+            df['threshold'] = df['Q{:.2f}'.format(BAD)] - df[
+                ['Q{:.2f}'.format(another_BAD) for another_BAD in states if another_BAD != BAD]].max(axis=1)
+            print(df['threshold'].unique())
+            df = df[['BAD', 'COSMIC', 'threshold']]
+            # min_tr = df['threshold'].min()
+            # max_tr = df['threshold'].max()
+            N = 1000
+            idxs = set(int(x) for x in np.linspace(0, len(df.index) - 1, N))
+            sorted_by_thresholds = df['threshold'].to_numpy(copy=True)
+            sorted_by_thresholds.sort()
+            print('isort')
+            thresholds = {0}
+            for index, value in enumerate(sorted_by_thresholds):
+                if index in idxs:
+                    thresholds.add(value)
+            thresholds = sorted(list(thresholds))
+            print(thresholds)
+            sum_df = None
+            for threshold in thresholds:
+                print('Now doing threshold = {}, BAD={:.2f}'.format(threshold, BAD))
+                before = len(df.index)
+                df = df[df['threshold'] >= threshold]
+                print('change: {}'.format(before - len(df.index)))
+                df_counts = df.groupby(['BAD', 'COSMIC']).size().reset_index(name='counts')
+                df_counts = df_counts.groupby(['BAD', 'COSMIC'], as_index=False)['counts'].sum()
+                df_counts['threshold'] = threshold
+                if sum_df is None:
+                    sum_df = df_counts
+                else:
+                    sum_df = sum_df.append(df_counts)
+
+            res_dir = os.path.expanduser('~/PARAMETERS/counts/')
+            sum_df.to_csv(os.path.join(res_dir, 'counts_deltaqm_{}_{}_{:.2f}.tsv'.format(cell_sign, model, BAD)),
+                          index=False, sep='\t')
