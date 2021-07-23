c638730681deba142c29dad629d33230b46780a3
wish1 <aswq22013@gmail.com>
2021-07-24 01:38:02 +0300

    stash CTCF

--- scripts/Qcontrol/stash_CTCF.py
+++ scripts/Qcontrol/stash_CTCF.py
@@ -7,6 +7,7 @@ alignments_path = os.path.expanduser('~/AlignmentsChip')
 master_list = pd.read_table('~/Configs/master-chip.txt')
 print(master_list)
 master_list = master_list[master_list['TF_UNIPROT_NAME'] == 'CTCF_HUMAN']
+master_list = master_list[master_list.apply(lambda row: os.path.exists(os.path.join(alignments_path, row['#EXP'], row['ALIGNS'] + '.table_p')), axis=1)]
 master_list.to_csv('~/Configs/CTCF_master.tsv', sep='\t', index=False)
 with ZipFile(os.path.expanduser('~/CTCF_dump.zip'), 'w') as zipObj2:
     for index, row in master_list.iterrows():
