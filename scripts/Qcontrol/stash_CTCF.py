bd90bb1e32c0729c56171204afbc60dd8d11bb6b
wish1 <aswq22013@gmail.com>
2021-07-24 01:36:19 +0300

    stash CTCF

--- scripts/Qcontrol/stash_CTCF.py
+++ scripts/Qcontrol/stash_CTCF.py
@@ -10,6 +10,6 @@ master_list = master_list[master_list['TF_UNIPROT_NAME'] == 'CTCF_HUMAN']
 master_list.to_csv('~/Configs/CTCF_master.tsv', sep='\t', index=False)
 with ZipFile(os.path.expanduser('~/CTCF_dump.zip'), 'w') as zipObj2:
     for index, row in master_list.iterrows():
-        path = os.path.join(alignments_path, row['#EXP'], row['ALIGNS'])
-        zipObj2.write(path, os.path.join('DATA', os.path.basename(path)))
+        path = os.path.join(alignments_path, row['#EXP'], row['ALIGNS'] + '.table_p')
+        zipObj2.write(path, os.path.join('DATA', os.path.splitext(os.path.basename(path))[0] + '.tsv'))
     zipObj2.write(os.path.expanduser('~/Configs/CTCF_master.tsv'), 'CTCF_master.tsv')
