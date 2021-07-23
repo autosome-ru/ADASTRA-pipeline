92535d2c712075f99bfa130eaa8ca19d8b5d8c9c
wish1 <aswq22013@gmail.com>
2021-07-24 01:28:50 +0300

    stash CTCF

--- scripts/Qcontrol/stash_CTCF.py
+++ scripts/Qcontrol/stash_CTCF.py
@@ -10,5 +10,5 @@ master_list.to_csv('~/Configs/CTCF_master.tsv', sep='\t', index=False)
 with ZipFile(os.path.expanduser('~/CTCF_dump.zip'), 'w') as zipObj2:
     for index, row in master_list.iterrows():
         path = os.path.join(alignments_path, row['#EXP'], row['ALIGNS'])
-        zipObj2.write(path, os.path.basename(path))
+        zipObj2.write(path, os.path.join('DATA', os.path.basename(path)))
     zipObj2.write('~/Configs/CTCF_master.tsv', 'CTCF_master.tsv')
