b47d89d23a4b8b7a897b8be192e5b2f0deea3aa9
wish1 <aswq22013@gmail.com>
2021-08-11 23:08:19 +0300

    new individual ref badmaps filter procedure

--- pipeline_start.sh
+++ pipeline_start.sh
@@ -87,36 +87,36 @@ if [ "$stage_index" -le 4 ]; then
 #    echo 'Correlation analysis failed'
 #    exit 1
 #  fi
-#  if ! adastra create_badmaps_filter
-#  then
-#    echo 'BAD maps filter creation failed'
-#    exit 1
-#  fi
-#  if ! adastra apply_badmaps_filter
-#  then
-#    echo 'BAD maps filter application failed'
-#    exit 1
-#  fi
-#  if ! bash "$scripts_path"/bad_map_est.sh "$njobs" --merge --remake
-#  then
-#    echo 'BAD estimation failed (iteration 2)'
-#    exit 1
-#  fi
-#  if ! bash "$scripts_path"/correlation_with_cosmic.sh "$njobs" --annotate --remake
-#  then
-#    echo 'Correlation analysis failed (iteration 2)'
-#    exit 1
-#  fi
-#  if ! adastra create_badmaps_filter --remake
-#  then
-#    echo 'BAD maps filter creation failed (iteration 2)'
-#    exit 1
-#  fi
-#  if ! adastra apply_badmaps_filter --remake
-#  then
-#    echo 'BAD maps filter application failed (iteration 2)'
-#    exit 1
-#  fi
+  if ! adastra create_badmaps_filter
+  then
+    echo 'BAD maps filter creation failed'
+    exit 1
+  fi
+  if ! adastra apply_badmaps_filter
+  then
+    echo 'BAD maps filter application failed'
+    exit 1
+  fi
+  if ! bash "$scripts_path"/bad_map_est.sh "$njobs" --merge --remake
+  then
+    echo 'BAD estimation failed (iteration 2)'
+    exit 1
+  fi
+  if ! bash "$scripts_path"/correlation_with_cosmic.sh "$njobs" --annotate --remake
+  then
+    echo 'Correlation analysis failed (iteration 2)'
+    exit 1
+  fi
+  if ! adastra create_badmaps_filter --remake
+  then
+    echo 'BAD maps filter creation failed (iteration 2)'
+    exit 1
+  fi
+  if ! adastra apply_badmaps_filter --remake
+  then
+    echo 'BAD maps filter application failed (iteration 2)'
+    exit 1
+  fi
   if ! bash "$scripts_path"/BAD_annotation.sh "$njobs" --remade
   then
     echo 'BAD annotation failed'
