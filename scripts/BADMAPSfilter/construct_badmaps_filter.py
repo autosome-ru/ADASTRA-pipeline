23d478a26ce852db80172e9d1ba724a2a4b228c8
Sashok <boytsovs@mail.ru>
2021-12-23 21:32:22 +0300

    debug

--- scripts/BADMAPSfilter/construct_badmaps_filter.py
+++ scripts/BADMAPSfilter/construct_badmaps_filter.py
@@ -113,7 +113,8 @@ def make_binom_density(cov, BAD, allele_tr):
     return res / norm
 
 
-def init_process_for_mode(mode, cors):
+def init_process_for_mode(args):
+    mode, cors = args
     test_dfs = open_dfs(cors, mode, remake=False, concat=False)
     print('Test concatenated {}'.format(mode))
     states = get_states_from_model_name(mode)
