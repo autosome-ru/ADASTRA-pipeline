7548ff50c4297529c2a143ed23de092060e12e7d
m1kss <boytsovs.av@phystech.edu>
2022-04-04 10:47:35 +0300

    mixalime integration

--- scripts/FITnoise/fit_dist.py
+++ scripts/FITnoise/fit_dist.py
@@ -8,7 +8,9 @@ from scripts.HELPERS.helpers import mixalime_params
 def main():
     in_dir = get_release_stats_path()
     args = (item for pair in mixalime_params.items() for item in pair)
-    pr = subprocess.run(['negbin_fit', '-O', in_dir, *args])
+    pr = subprocess.run(['negbin_fit', '-O', in_dir, *args],
+                        check=True,
+                        capture_output=True)
     err = pr.stderr
     if err:
         err = err.decode('utf-8')
