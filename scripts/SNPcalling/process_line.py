91b4199e997dfdf284494af4bc25581bef23601b
wishabc <aswq22013@gmail.com>
2022-12-28 22:19:32 -0800

    new faire branch added

--- scripts/SNPcalling/process_line.py
+++ scripts/SNPcalling/process_line.py
@@ -4,7 +4,7 @@ import subprocess
 from scripts.HELPERS.paths_for_components import alignments_path
 
 
-dnase_bams_path = '/mnt/NAS/home/abramov/raw_alignments.GTRD/dnase'
+dnase_bams_path = '/mnt/NAS/home/abramov/raw_alignments.GTRD/faire'
 
 
 def make_bams_list(gtrd_id):
@@ -13,7 +13,7 @@ def make_bams_list(gtrd_id):
 
 
 def process_bam(bam, out_path):
-    cmd = ['bash', '/home/abramov/dnase/ADASTRA-pipeline/scripts/SNPcalling/SNPcalling.sh',
+    cmd = ['bash', '/home/abramov/faire/ADASTRA-pipeline/scripts/SNPcalling/SNPcalling.sh',
            '-Exp', os.path.join(dnase_bams_path, bam),
            '-Out', out_path]
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
